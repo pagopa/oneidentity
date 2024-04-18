00c20797c083cc8bbbd32349b940c9332b0b725b
uolter <walter.traspadini@pagopa.it>
2024-04-18 16:47:14 +0200

    assertion enable bucket key

--- src/infra/modules/storage/main.tf
+++ src/infra/modules/storage/main.tf
@@ -27,6 +27,7 @@ module "s3_assetion_bucket" {
 
   server_side_encryption_configuration = {
     rule = {
+      bucket_key_enabled = true
       apply_server_side_encryption_by_default = {
         kms_master_key_id = aws_kms_key.kms_assetion_bucket.arn
         sse_algorithm     = "aws:kms"
