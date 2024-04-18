fdb5458f34a30ceb3ac9de3d8e508bf794a64d91
uolter <walter.traspadini@pagopa.it>
2024-04-18 15:57:22 +0200

    encrypt s3 bucket with kms key

--- src/infra/modules/storage/variables.tf
+++ src/infra/modules/storage/variables.tf
@@ -1,8 +1,9 @@
 variable "assertion_bucket" {
   type = object({
-    name_prefix              = string
-    gracier_transaction_days = number
-    expiration_days          = number
-    mfa_delete               = optional(bool, false)
+    name_prefix                     = string
+    gracier_transaction_days        = number
+    expiration_days                 = number
+    mfa_delete                      = optional(bool, false)
+    kms_key_deletion_window_in_days = optional(number, 10)
   })
 }
