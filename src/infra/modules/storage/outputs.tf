a365c9422577aa3849a94c22dbc1707eb52ba815
uolter <walter.traspadini@pagopa.it>
2024-04-22 11:26:38 +0200

    update storage module outputs

--- src/infra/modules/storage/outputs.tf
+++ src/infra/modules/storage/outputs.tf
@@ -1,3 +1,7 @@
-output "s3_assetion_bucket" {
+output "assertion_bucket_arn" {
   value = module.s3_assetion_bucket.s3_bucket_arn
+}
+
+output "assertion_bucket_name" {
+  value = module.s3_assetion_bucket.s3_bucket_id
 }
