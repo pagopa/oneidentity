a3fe137273b1c40fb95b2ddd384742edafa2dd86
BenitoVisone <visonebenito@gmail.com>
2024-05-16 17:48:54 +0200

    feat: setup project structure

---
+++ src/1id/1id-ecs-core/README.md
@@ -0,0 +1,1 @@
+## Do not use dockerfiles inside this maven module.
---
+++ src/1id/1id-ecs-core/src/test/java/it/pagopa/oneid/OneIDIT.java
@@ -0,0 +1,8 @@
+package it.pagopa.oneid;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class OneIDIT extends OneIDTest {
+    // Execute the same tests but in packaged mode.
+}
