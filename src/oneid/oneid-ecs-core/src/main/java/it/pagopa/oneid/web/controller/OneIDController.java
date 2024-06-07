3ae4583cb60a874a0a344cc4d8786ecfc66ef0c8
BenitoVisone <visonebenito@gmail.com>
2024-06-07 17:19:51 +0200

    feat: setup mock controller response

--- src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/web/controller/OneIDController.java
+++ src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/web/controller/OneIDController.java
@@ -1,10 +1,11 @@
 package it.pagopa.oneid.web.controller;
 
 import it.pagopa.oneid.web.dto.AuthorizationRequestDTOExtended;
-import jakarta.validation.Valid;
 import jakarta.ws.rs.BeanParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 
@@ -20,7 +21,44 @@ public class OneIDController {
 
     @GET
     @Path("/login")
-    public Response login(@BeanParam @Valid AuthorizationRequestDTOExtended authorizationRequestDTOExtended) {
-        return Response.ok("Login Path: " + authorizationRequestDTOExtended.getIdp() + " ").build();
+    @Produces(MediaType.TEXT_HTML)
+    public Response login(@BeanParam AuthorizationRequestDTOExtended authorizationRequestDTOExtended) {
+        // TODO remove this Mock and set @Valid
+        String localIDP = "https://localhost:8443/samlsso";
+        String localDemoIDP = "https://localhost:8443/demo/samlsso";
+        return Response.ok("    <script src=\"https://italia.github.io/spid-smart-button/spid-button.min.js\"></script>\n" +
+                "    <div id=\"spid-button\">\n" +
+                "      <noscript>\n" +
+                "          Il login tramite SPID richiede che JavaScript sia abilitato nel browser.\n" +
+                "      </noscript>\n" +
+                "    </div>\n" +
+                "    <script>\n" +
+                "      SPID.init({\n" +
+                "        url: '/authorize?idp={{idp}}',\n" +
+                "        supported: [\n" +
+                "            'https://demo.spid.gov.it/',\n" +
+                "        ],\n" +
+                "        extraProviders: [{\n" +
+                "            \"protocols\": [\"SAML\"],\n" +
+                "            \"entityName\": \"IDP test\",\n" +
+                "            \"logo\": \"spid-idp-aruba.svg\",\n" +
+                "            \"entityID\": \"" + localIDP + "\",\n" +
+                "            \"active\": true\n" +
+                "        },{\n" +
+                "          \"protocols\": [\"SAML\"],\n" +
+                "          \"entityName\": \"IDP demo\",\n" +
+                "          \"logo\": \"spid-idp-aruba.svg\",\n" +
+                "          \"entityID\": \"https://demo.spid.gov.it/\",\n" +
+                "          \"active\": true\n" +
+                "        },{\n" +
+                "          \"protocols\": [\"SAML\"],\n" +
+                "          \"entityName\": \"IDP local demo\",\n" +
+                "          \"logo\": \"spid-idp-aruba.svg\",\n" +
+                "            \"entityID\": \"" + localDemoIDP + "\",\n" +
+                "          \"active\": true\n" +
+                "        }," +
+                "]\n" +
+                "      });\n" +
+                "    </script>").build();
     }
 }
