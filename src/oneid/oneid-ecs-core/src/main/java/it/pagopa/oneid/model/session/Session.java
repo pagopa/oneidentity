ffab98e06cab14727598e1afa2c0ac0d7876200c
Benito Visone <visonebenito@gmail.com>
2024-06-07 14:36:04 +0200

    feat: setup dynamodb interaction (#76)

--- src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/model/session/Session.java
+++ src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/model/session/Session.java
@@ -2,11 +2,19 @@ package it.pagopa.oneid.model.session;
 
 import it.pagopa.oneid.model.session.enums.RecordType;
 import lombok.Data;
+import lombok.Getter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
 
+@DynamoDbBean
 @Data
-public class Session {
+public abstract class Session {
+
+    @Getter(onMethod_ = @DynamoDbPartitionKey)
     private String SAMLRequestID;
 
+    @Getter(onMethod_ = @DynamoDbSortKey)
     private RecordType recordType;
 
     private Long creationTime;
