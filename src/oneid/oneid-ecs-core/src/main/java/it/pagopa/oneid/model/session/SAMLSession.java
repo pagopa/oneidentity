4b6ea58d897e08298dbba28987a355af0872490c
BenitoVisone <visonebenito@gmail.com>
2024-06-11 11:22:13 +0200

    feat: add constructor with only SAMLRequest parameter, AllArgs and NoArgs for SAMLSession

--- src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/model/session/SAMLSession.java
+++ src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/model/session/SAMLSession.java
@@ -1,15 +1,27 @@
 package it.pagopa.oneid.model.session;
 
+import it.pagopa.oneid.model.session.enums.RecordType;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 
 @EqualsAndHashCode(callSuper = true)
 @DynamoDbBean
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SAMLSession extends Session {
 
-    private String SAMLRequest; //TODO change in ?
+    @NotNull
+    private String SAMLRequest;
 
-    private String SAMLResponse; //TODO change in ?
+    private String SAMLResponse;
+
+    public SAMLSession(@NotNull String SAMLRequestID, @NotNull RecordType recordType, @NotNull long creationTime, @NotNull long ttl, String SAMLRequest) {
+        super(SAMLRequestID, recordType, creationTime, ttl);
+        this.SAMLRequest = SAMLRequest;
+    }
 }
