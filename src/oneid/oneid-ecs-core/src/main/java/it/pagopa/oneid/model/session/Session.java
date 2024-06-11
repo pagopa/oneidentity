1055862ff9c17114998bc38e0a0bb1b89211ffc5
BenitoVisone <visonebenito@gmail.com>
2024-06-11 11:21:20 +0200

    feat: set long type to creationTimea and TTl and add AllArgs and NoArgs constructors for Session

--- src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/model/session/Session.java
+++ src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/model/session/Session.java
@@ -1,23 +1,32 @@
 package it.pagopa.oneid.model.session;
 
 import it.pagopa.oneid.model.session.enums.RecordType;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
 
 @DynamoDbBean
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public abstract class Session {
 
     @Getter(onMethod_ = @DynamoDbPartitionKey)
+    @NotNull
     private String SAMLRequestID;
 
     @Getter(onMethod_ = @DynamoDbSortKey)
+    @NotNull
     private RecordType recordType;
 
-    private Long creationTime;
+    @NotNull
+    private long creationTime;
 
-    private Long ttl;
+    @NotNull
+    private long ttl;
 }
