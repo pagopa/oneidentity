c690cab875bac353e1bc6c875d6c72f5ba891e0a
Alessandro Sebastiani <sebbalex@gmail.com>
2024-06-13 17:15:38 +0200

    feat: add web bundler, serve static html page through /saml/acs route

--- src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/web/controller/SAMLController.java
+++ src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/web/controller/SAMLController.java
@@ -13,39 +13,36 @@ import java.net.URISyntaxException;
 @Path(("/saml"))
 public class SAMLController {
 
-    @POST
-    @Path("/acs")
-    @Produces(MediaType.TEXT_HTML)
+  @POST
+  @Path("/acs")
+  @Produces(MediaType.TEXT_HTML)
 
-    public Response samlACS(@BeanParam SAMLResponseDTO samlResponseDTO) {
-        // TODO Remove mock and set @Valid param
-        if (samlResponseDTO.getSAMLResponse().equals("test")) {
-            Response redirectResponse = null;
-            try {
-                redirectResponse = Response
-                        .status(302)
-                        .location(new URI("/login"))
-                        .build();
-            } catch (URISyntaxException e) {
-                throw new RuntimeException(e);
-            }
-            return redirectResponse;
-        } else return Response.ok("<html lang=\"it\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                "    <title>HTML di esempio</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "    <h1>HTML di esempio</h1>\n" +
-                "</body>\n" +
-                "</html>").build();
+  public Response samlACS(@BeanParam SAMLResponseDTO samlResponseDTO) {
+    // TODO Remove mock and set @Valid param
+    if ((samlResponseDTO.getSAMLResponse() != null) && (samlResponseDTO.getSAMLResponse().equals("test"))) {
+      try {
+        return Response
+            .status(302)
+            .location(new URI("/login"))
+            .build();
+      } catch (URISyntaxException e) {
+        // throw new RuntimeException(e);
+        return Response.status(400).build();
+      }
+    } else {
+      try {
+        return Response.status(302).location(new URI("/static/sample_index.html")).build();
+      } catch (URISyntaxException e) {
+        // throw new RuntimeException(e);
+        return Response.status(400).build();
+      }
     }
+  }
 
-    @GET
-    @Path("/assertion")
-    public Response assertion(@BeanParam @Valid AccessTokenDTO accessToken) {
-        return Response.ok("Assertion Path").build();
-    }
+  @GET
+  @Path("/assertion")
+  public Response assertion(@BeanParam @Valid AccessTokenDTO accessToken) {
+    return Response.ok("Assertion Path").build();
+  }
 
 }
