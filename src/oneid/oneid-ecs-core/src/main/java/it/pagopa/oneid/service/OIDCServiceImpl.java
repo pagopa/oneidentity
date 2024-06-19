93ca36560ffab0c80a00fa8a53ac98e304e1d77a
BenitoVisone <visonebenito@gmail.com>
2024-06-19 16:59:49 +0200

    feat: bootstrap OIDCServiceImpl

---
+++ src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/service/OIDCServiceImpl.java
@@ -0,0 +1,86 @@
+package it.pagopa.oneid.service;
+
+import com.nimbusds.oauth2.sdk.AuthorizationCode;
+import com.nimbusds.oauth2.sdk.AuthorizationRequest;
+import com.nimbusds.oauth2.sdk.AuthorizationResponse;
+import com.nimbusds.oauth2.sdk.ResponseType;
+import com.nimbusds.oauth2.sdk.Scope;
+import com.nimbusds.oauth2.sdk.TokenResponse;
+import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.oauth2.sdk.id.State;
+import com.nimbusds.openid.connect.sdk.AuthenticationSuccessResponse;
+import it.pagopa.oneid.common.Client;
+import it.pagopa.oneid.model.dto.AttributeDTO;
+import it.pagopa.oneid.model.dto.AuthorizationRequestDTO;
+import jakarta.enterprise.context.ApplicationScoped;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@ApplicationScoped
+public class OIDCServiceImpl implements OIDCService {
+
+  @Override
+  public Client getClientRegistration(String clientID) {
+    return null;
+  }
+
+  @Override
+  public AuthorizationRequest buildAuthorizationRequest(
+      AuthorizationRequestDTO authorizationRequestDTO) {
+
+    // The client identifier provisioned by the server
+    ClientID clientID = new ClientID(authorizationRequestDTO.getClientId());
+
+    // The requested scope values for the token
+    Scope scope = new Scope(authorizationRequestDTO.getScope());
+
+    // The client callback URI, typically pre-registered with the server
+    URI callback = null;
+    try {
+      callback = new URI(authorizationRequestDTO.getScope());
+    } catch (URISyntaxException e) {
+      throw new RuntimeException(e);
+    }
+
+    // Generate random state string for pairing the response to the request
+    State stateObj = new State(authorizationRequestDTO.getState());
+
+    // Build the request
+
+    return new AuthorizationRequest.Builder(
+        new ResponseType(authorizationRequestDTO.getResponseType().getValue()), clientID)
+        .scope(scope)
+        .state(stateObj)
+        .redirectionURI(callback)
+        .build();
+  }
+
+  @Override
+  public AuthorizationResponse getAuthorizationResponse(AuthorizationRequest authorizationRequest) {
+    // TODO lookup the client
+    ClientID clientID = authorizationRequest.getClientID();
+
+    // The client callback URL, must be registered in the server's database
+    URI callback = authorizationRequest.getRedirectionURI();
+
+    // The state, must be echoed back with the response
+    State state = authorizationRequest.getState();
+
+    // The requested scope
+    // TODO how to handle this ?
+    Scope scope = authorizationRequest.getScope();
+
+    // Generate the response...
+
+    AuthorizationCode code = new AuthorizationCode();
+
+    return new AuthenticationSuccessResponse(callback, code, null, null, state, null, null);
+
+  }
+
+  @Override
+  public TokenResponse getTokenResponse(List<AttributeDTO<?>> attributeDTO) {
+    return null;
+  }
+}
