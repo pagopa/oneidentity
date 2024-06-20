60c2dcd6f9f8bfb27a0dbeafc86a287fb2837ab7
giuseppe-gangemi <giuseppe.gangemi@pagopa.it>
2024-06-20 11:08:09 +0200

    feat: add saveSession for OIDC record

--- src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/connector/SessionConnectorImpl.java
+++ src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/connector/SessionConnectorImpl.java
@@ -1,5 +1,6 @@
 package it.pagopa.oneid.connector;
 
+import io.quarkus.logging.Log;
 import it.pagopa.oneid.exception.SessionException;
 import it.pagopa.oneid.model.session.AccessTokenSession;
 import it.pagopa.oneid.model.session.OIDCSession;
@@ -7,10 +8,15 @@ import it.pagopa.oneid.model.session.SAMLSession;
 import it.pagopa.oneid.model.session.Session;
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Expression;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.model.PutItemEnhancedRequest;
 
 @Dependent
 public class SessionConnectorImpl<T extends Session> implements SessionConnector<T> {
@@ -20,9 +26,9 @@ public class SessionConnectorImpl<T extends Session> implements SessionConnector
   private static final String GSI_IDX_NAME = "gsi_code_idx";
 
 
+  private final DynamoDbTable<SAMLSession> samlSessionMapper;
   private final DynamoDbTable<OIDCSession> oidcSessionMapper;
   private final DynamoDbTable<AccessTokenSession> accessTokenSessionMapper;
-  private final DynamoDbTable<SAMLSession> samlSessionMapper;
 
   @Inject
   SessionConnectorImpl(DynamoDbEnhancedClient dynamoDbEnhancedClient) {
@@ -36,9 +42,45 @@ public class SessionConnectorImpl<T extends Session> implements SessionConnector
 
   @Override
   public void saveSession(T session) throws SessionException {
+    Log.debug("[SessionConnectorImpl.saveSession] start");
+    // TODO: find a better way to avoid duplication
     switch (session) {
-      case SAMLSession samlSession -> samlSessionMapper.putItem(samlSession);
-      case OIDCSession oidcSession -> oidcSessionMapper.putItem(oidcSession);
+      case SAMLSession samlSession -> {
+        try {
+          samlSessionMapper.putItem(
+              PutItemEnhancedRequest.builder(SAMLSession.class)
+                  .item(samlSession)
+                  .conditionExpression(
+                      Expression.builder().expression(
+                              "attribute_not_exists(SAMLRequestID)")
+                          .build())
+                  .build());
+        } catch (ConditionalCheckFailedException e) {
+          Log.debug(
+              "[SessionConnectorImpl.saveSession] a record with the same SAMLRequestID already exists");
+          throw new SessionException("Existing SAMLSession for the specified SAMLRequestID");
+        }
+        Log.debug("[SessionConnectorImpl.saveSession] successfully saved SamlSession");
+      }
+
+      case OIDCSession oidcSession -> {
+        try {
+          oidcSessionMapper.putItem(
+              PutItemEnhancedRequest.builder(OIDCSession.class)
+                  .item(oidcSession)
+                  .conditionExpression(
+                      Expression.builder().expression(
+                              "attribute_not_exists(SAMLRequestID)")
+                          .build())
+                  .build());
+
+        } catch (ConditionalCheckFailedException e) {
+          Log.debug(
+              "[SessionConnectorImpl.saveSession] a record with the same SAMLRequestID already exists");
+          throw new SessionException("Existing OIDCSession for the specified SAMLRequestID");
+        }
+        Log.debug("[SessionConnectorImpl.saveSession] successfully saved OIDCSession");
+      }
       case AccessTokenSession accessTokenSession -> {
         //TODO implement
         break;
--- src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/service/SessionServiceImpl.java
+++ src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/service/SessionServiceImpl.java
@@ -1,5 +1,6 @@
 package it.pagopa.oneid.service;
 
+import io.quarkus.logging.Log;
 import it.pagopa.oneid.connector.SessionConnectorImpl;
 import it.pagopa.oneid.exception.SessionException;
 import it.pagopa.oneid.model.session.Session;
@@ -16,9 +17,12 @@ public class SessionServiceImpl<T extends Session> implements SessionService<T> 
 
   @Override
   public void saveSession(T session) throws SessionException {
+    Log.debug("[SessionServiceImpl.saveSession] start");
     if (session != null) {
+      Log.debug("[SessionServiceImpl.saveSession] session successfully saved");
       sessionConnectorImpl.saveSession(session);
     } else {
+      Log.debug("[SessionServiceImpl.saveSession] save failed");
       throw new SessionException();
     }
   }
