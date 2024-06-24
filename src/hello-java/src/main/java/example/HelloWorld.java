447c2da00e67ccefca930434cb884e09c3470c2d
uolter <walter.traspadini@pagopa.it>
2024-06-24 18:36:37 +0200

    rollback java code hello world

--- src/hello-java/src/main/java/example/HelloWorld.java
+++ src/hello-java/src/main/java/example/HelloWorld.java
@@ -2,23 +2,10 @@ package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import org.json.JSONObject;
-import java.util.HashMap;
-import java.util.Map;
 
-public class HelloWorld implements RequestHandler<Object, APIGatewayProxyResponseEvent> {
-
+public class HelloWorld implements RequestHandler<Object, String> {
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
-        JSONObject responseBody = new JSONObject();
-        responseBody.put("message", "Hello, World!");
-
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-        response.setStatusCode(200);
-        response.setHeaders(Map.of("Content-Type", "application/json"));
-        response.setBody(responseBody.toString());
-
-        return response;
+    public String handleRequest(Object input, Context context) {
+        return "Hello, World!";
     }
 }
