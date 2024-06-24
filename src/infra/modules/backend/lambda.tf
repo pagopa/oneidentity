488150fa487f2d2e1388a78ddf59532bf7c2e8e8
uolter <walter.traspadini@pagopa.it>
2024-06-24 10:04:35 +0200

    refactoring module backend

--- src/infra/modules/backend/lambda.tf
+++ src/infra/modules/backend/lambda.tf
@@ -1,3 +1,51 @@
+## Github 
+
+resource "aws_iam_role_policy_attachment" "deploy_lambda" {
+  role       = aws_iam_role.github_lambda_deploy.name
+  policy_arn = aws_iam_policy.deploy_lambda.id
+
+}
+
+## Deploy with github action
+resource "aws_iam_role" "githubecsdeploy" {
+  name        = format("%s-deploy", var.service_core.service_name)
+  description = "Role to assume to deploy ECS tasks"
+
+
+  assume_role_policy = local.assume_role_policy_github
+}
+
+resource "aws_iam_role" "github_lambda_deploy" {
+  name        = "GitHubDeployLambda"
+  description = "Role to deploy lambda functions with github actions."
+
+
+  assume_role_policy = local.assume_role_policy_github
+}
+
+resource "aws_iam_policy" "deploy_lambda" {
+  name        = "DeployLambda"
+  description = "Policy to deploy Lambda functions"
+
+  policy = jsonencode({
+
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "lambda:CreateFunction",
+          "lambda:UpdateFunctionCode",
+          "lambda:UpdateFunctionConfiguration"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+
+}
+
+
 ## client registration lambda
 
 data "aws_iam_policy_document" "client_registration_lambda" {
@@ -36,49 +84,4 @@ module "client_registration_lambda" {
   memory_size = 128
   timeout     = 30
 
-}
-
-resource "aws_iam_role" "github_lambda_deploy" {
-  name        = "GitHubDeployLambda"
-  description = "Role to deploy lambda functions with github actions."
-
-
-  assume_role_policy = local.assume_role_policy_github
-}
-
-resource "aws_iam_policy" "deploy_lambda" {
-  name        = "DeployLambda"
-  description = "Policy to deploy Lambda functions"
-
-  policy = jsonencode({
-
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "lambda:CreateFunction",
-          "lambda:UpdateFunctionCode",
-          "lambda:UpdateFunctionConfiguration"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-
-}
-
-resource "aws_iam_role_policy_attachment" "deploy_lambda" {
-  role       = aws_iam_role.github_lambda_deploy.name
-  policy_arn = aws_iam_policy.deploy_lambda.id
-
-}
-
-## Deploy with github action
-resource "aws_iam_role" "githubecsdeploy" {
-  name        = format("%s-deploy", var.service_core.service_name)
-  description = "Role to assume to deploy ECS tasks"
-
-
-  assume_role_policy = local.assume_role_policy_github
 }
