48d5379fecbcd90e57d4110b19bd9185dba0b0a2
uolter <walter.traspadini@pagopa.it>
2024-06-24 17:02:32 +0200

    update java and build

--- src/hello-java/src/main/java/example/HelloWorld.java
+++ src/hello-java/src/main/java/example/HelloWorld.java
@@ -2,19 +2,18 @@ package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import org.json.simple.JSONObject;
+import com.google.gson.Gson;
+import java.util.HashMap;
 import java.util.Map;
 
-public class HelloWorld implements RequestHandler<Map<String, Object>, String>{
-    
+public class HelloWorld implements RequestHandler<Object, String> {
     @Override
-    public String handleRequest(Map<String, Object> stringStringMap, Context context) {
-        JSONObject obj = new JSONObject();
-        JSONObject obj2 = new JSONObject();
-        obj2.put("Content-Type", "application/json");
-        obj.put("statusCode", 200);
-        obj.put("headers", obj2);
-        obj.put("body", "hello world");
-        return obj.toString();
+    public String handleRequest(Object input, Context context) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, World!");
+
+        // Convert the map to a JSON string
+        Gson gson = new Gson();
+        return gson.toJson(response);
     }
 }
