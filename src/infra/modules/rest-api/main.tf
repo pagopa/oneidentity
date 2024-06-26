88afbb99098c17ed3a26fda3f1886290b8f8a4d2
uolter <walter.traspadini@pagopa.it>
2024-06-26 16:48:16 +0200

    first draft method settins for caching

--- src/infra/modules/rest-api/main.tf
+++ src/infra/modules/rest-api/main.tf
@@ -35,6 +35,9 @@ resource "aws_api_gateway_stage" "main" {
   rest_api_id   = aws_api_gateway_rest_api.main.id
   stage_name    = var.stage_name
 
+  cache_cluster_enabled = var.cache_cluster_enabled
+  cache_cluster_size    = var.cache_cluster_size
+
   tags = {
     Name = format("%s-%s", var.name, var.stage_name)
   }
