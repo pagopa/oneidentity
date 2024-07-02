427be36262d79f2a693e0cec6450436ef72f8b1e
Benito Visone <visonebenito@gmail.com>
2024-07-02 11:21:29 +0200

    Feat: add saml controller assertion route (#150)
    * feat: add saml/assertion route
    
    * chore: add saml/assertion route logs
    
    * feat: edit api gateway openapi spec to meet saml/assertion requirements

--- src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/service/SessionServiceImpl.java
+++ src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/service/SessionServiceImpl.java
@@ -3,7 +3,6 @@ package it.pagopa.oneid.service;
 import io.quarkus.logging.Log;
 import it.pagopa.oneid.connector.SessionConnectorImpl;
 import it.pagopa.oneid.exception.SessionException;
-import it.pagopa.oneid.model.session.OIDCSession;
 import it.pagopa.oneid.model.session.SAMLSession;
 import it.pagopa.oneid.model.session.Session;
 import it.pagopa.oneid.model.session.enums.RecordType;
@@ -37,17 +36,31 @@ public class SessionServiceImpl<T extends Session> implements SessionService<T> 
 
   @Override
   public void setSAMLResponse(String samlRequestID, String response) throws SessionException {
+    Log.debug("[SessionServiceImpl.setSAMLResponse] start");
     sessionConnectorImpl.updateSAMLSession(samlRequestID, response);
   }
 
   @Override
   public SAMLSession getSAMLSessionByCode(String code) throws SessionException {
+    Log.debug("[SessionServiceImpl.getSAMLSessionByCode] start");
+    return getSAMLSessionByCodeAndRecord(code, RecordType.OIDC);
+  }
 
-    OIDCSession oidcSession = (OIDCSession) sessionConnectorImpl.findSession(code, RecordType.OIDC)
+  @Override
+  public String getSAMLResponseByCode(String code) throws SessionException {
+    Log.debug("[SessionServiceImpl.getSAMLResponseByCode] start");
+    return getSAMLSessionByCodeAndRecord(code, RecordType.ACCESS_TOKEN).getSAMLResponse();
+  }
+
+  private SAMLSession getSAMLSessionByCodeAndRecord(String code, RecordType recordType)
+      throws SessionException {
+    Log.debug("[SessionServiceImpl.getSAMLSessionByCodeAndRecord] start");
+    T oidcSession = sessionConnectorImpl.findSession(code, recordType)
         .orElseThrow(SessionException::new);
-
+    Log.debug("[SessionServiceImpl.getSAMLSessionByCodeAndRecord] end");
     return (SAMLSession) sessionConnectorImpl.findSession(
         oidcSession.getSamlRequestID(),
         RecordType.SAML).orElseThrow(SessionException::new);
   }
+
 }
