c08f4ae4664d77b96b0e941c38b1c98f3f7ea06e
GiuMontesano <giuseppe.montesano@pagopa.it>
2024-07-12 12:35:07 +0200

    feat: add controller layer bootstrap

---
+++ src/oneid/oneid-lambda-client-registration/src/main/java/it/pagopa/oneid/web/controller/ClientRegistrationController.java
@@ -0,0 +1,26 @@
+package it.pagopa.oneid.web.controller;
+
+import it.pagopa.oneid.model.dto.ClientRegistrationRequestDTO;
+import it.pagopa.oneid.service.ClientRegistrationServiceImpl;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/")
+public class ClientRegistrationController {
+
+  @Inject
+  ClientRegistrationServiceImpl clientRegistrationService;
+
+  @POST
+  @Path("/connect/register")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response register(ClientRegistrationRequestDTO clientRegistrationRequestDTO) {
+    //TODO implement
+    return Response.ok("Register path").build();
+  }
+
+}
