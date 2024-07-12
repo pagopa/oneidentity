c1914cc7ffa4418d7b0df8a4fc9e3c8ace358960
GiuMontesano <giuseppe.montesano@pagopa.it>
2024-07-12 12:34:49 +0200

    feat: add service layer bootstrap

---
+++ src/oneid/oneid-lambda-client-registration/src/main/java/it/pagopa/oneid/service/ClientRegistrationServiceImpl.java
@@ -0,0 +1,18 @@
+package it.pagopa.oneid.service;
+
+import it.pagopa.oneid.model.dto.ClientRegistrationRequestDTO;
+
+public class ClientRegistrationServiceImpl implements ClientRegistrationService {
+
+  @Override
+  public void validateClientRegistrationInfo(
+      ClientRegistrationRequestDTO clientRegistrationRequestDTO) {
+    //TODO implement
+  }
+
+  @Override
+  public void saveClient(ClientRegistrationRequestDTO clientRegistrationRequestDTO) {
+    //TODO implement
+  }
+
+}
