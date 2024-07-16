df27a04bb16bc4cfcbca6e4adbb33bf0ade187a1
Giuseppe Montesano <104127973+GiuMontesano@users.noreply.github.com>
2024-07-16 16:45:12 +0200

    Feat: client registration connector (#183)
    * feat: add HASHUtils.generateRandomString method
    
    * feat: implement saveClientIfNotExists method

--- src/oneid/oneid-common/connector/src/main/java/it/pagopa/oneid/common/connector/ClientConnector.java
+++ src/oneid/oneid-common/connector/src/main/java/it/pagopa/oneid/common/connector/ClientConnector.java
@@ -1,6 +1,7 @@
 package it.pagopa.oneid.common.connector;
 
 import it.pagopa.oneid.common.model.Client;
+import it.pagopa.oneid.common.model.ClientExtended;
 import it.pagopa.oneid.common.model.dto.SecretDTO;
 import java.util.ArrayList;
 import java.util.Optional;
@@ -10,7 +11,6 @@ public interface ClientConnector {
   Optional<ArrayList<Client>> findAll();
 
   Optional<SecretDTO> getClientSecret(String clientId);
-
-  //TODO add Exception
-  void saveClientIfNotExists(Client client);
+  
+  void saveClientIfNotExists(ClientExtended client);
 }
