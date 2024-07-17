1eee7fd5398bdabbf93c38d1949cde79fee0534a
Walter Traspadini <walter.traspadini@pagopa.it>
2024-07-17 17:51:55 +0200

    spid validator env veriables (#189)

--- src/infra/modules/spid-validator/main.tf
+++ src/infra/modules/spid-validator/main.tf
@@ -63,6 +63,8 @@ module "ecs_spid_validator" {
         }
       ]
 
+      environment = var.spid_validator.container.environment
+
       log_configuration = {
         logDriver = "awslogs"
         options = {
