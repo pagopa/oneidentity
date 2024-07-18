6ca0654cec52293d919bf9aa5287035849f31409
uolter <walter.traspadini@pagopa.it>
2024-07-18 14:21:48 +0200

    first draft athena query

--- src/infra/modules/storage/main.tf
+++ src/infra/modules/storage/main.tf
@@ -5,7 +5,9 @@ resource "random_integer" "assertion_bucket_suffix" {
 
 locals {
   bucket_name = format("%s-%s", var.assertion_bucket.name_prefix,
-  random_integer.assertion_bucket_suffix.result)
+    random_integer.assertion_bucket_suffix.result
+  )
+  athena_outputs = format("query-%s", local.bucket_name)
 
 }
 
@@ -47,11 +49,6 @@ module "s3_assertions_bucket" {
     mfa_delete = var.assertion_bucket.mfa_delete
   }
 
-  logging = {
-    target_bucket = local.bucket_name
-    target_prefix = "logs/"
-  }
-
   lifecycle_rule = [
     {
       enabled = true
@@ -75,4 +72,74 @@ module "s3_assertions_bucket" {
   tags = {
     Name = local.bucket_name
   }
+}
+
+## Athena ##
+
+module "s3_athena_output_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.1"
+
+  bucket = local.athena_outputs
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  server_side_encryption_configuration = {
+    rule = {
+      bucket_key_enabled = true
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = module.kms_assertions_bucket.aliases["assertions/S3"].arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  tags = {
+    Name = local.bucket_name
+  }
+}
+
+resource "aws_athena_workgroup" "assertions_workgroup" {
+  name = "assertions_workgroup"
+
+  configuration {
+    result_configuration {
+      output_location = "s3://${local.athena_outputs}/output/"
+    }
+  }
+}
+
+# Create Athena database
+resource "aws_athena_database" "assertions" {
+  name   = "assertions"
+  bucket = module.s3_athena_output_bucket.s3_bucket_id
+}
+
+# Create Athena table
+resource "aws_athena_named_query" "create_assertions_table" {
+  name     = "create_assertions_table"
+  database = aws_athena_database.assertions.name
+  workgroup = aws_athena_workgroup.assertions_workgroup.id
+  query    = <<EOF
+CREATE EXTERNAL TABLE `auth_data`(
+  `samlRequestID` string, 
+  `creationTime` bigint', 
+  `recordType` string', 
+  `ttl` bigint)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'ignore.malformed.json'='true') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat'
+LOCATION
+  's3://assertions-6284/'
+TBLPROPERTIES (
+  'has_encrypted_data'='true', 
+  'transient_lastDdlTime'='1721301313')
+EOF
 }
