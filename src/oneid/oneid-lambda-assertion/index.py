d69a54d6d3cd74b531c08c2db6b96c4abcfb3522
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2024-07-18 14:42:16 +0200

    fix: added function to decode saml (#188)
    * fix: added function to decode saml
    
    * refactor: edit assertion lambda code
    
    * small refactory.
    
    * linting lambda function
    
    ---------
    
    Co-authored-by: uolter <walter.traspadini@pagopa.it>

--- src/oneid/oneid-lambda-assertion/index.py
+++ src/oneid/oneid-lambda-assertion/index.py
@@ -4,6 +4,7 @@ import logging
 import json
 from datetime import datetime, timedelta, timezone
 import dateutil.tz
+import base64
 
 
 logger = logging.getLogger()
@@ -12,37 +13,43 @@ logger.setLevel(logging.INFO)
 s3 = boto3.resource("s3")
 bucket_name = os.environ['S3_BUCKET']
 
+def decode_base64_content(content_body):
+    
+    samlreq_base64 = content_body['SAMLRequest']['S']
+    content_body['SAMLRequest'] = base64.b64decode(samlreq_base64).decode('utf-8')
+        
+    samlresp_base64 = content_body['SAMLResponse']['S']
+    content_body['SAMLResponse'] = base64.b64decode(samlresp_base64).decode('utf-8')
+
+    return content_body
+
 def convert_to_cet(creation_time):
     # Convert epoch time to UTC
     timezone = dateutil.tz.gettz('Europe/Rome')
     # Convert UTC to Central European Time (CET/CEST)
-    cet_time = datetime.fromtimestamp(creation_time, tz=timezone)
-    return cet_time
+    return datetime.fromtimestamp(creation_time, tz=timezone)
+    
 
 def lambda_handler(event, context):
 
-    for record in event:
-        
-        saml_request_id = record['dynamodb']['NewImage']['samlRequestID']['S']
-        record_type = record['dynamodb']['NewImage']['recordType']['S']
-        creation_time = record['dynamodb']['NewImage']['creationTime']['N']
-        contentBody = record['dynamodb']['NewImage']
-        cet_time = convert_to_cet(int(creation_time))
-       
-       
-    # Convert data to JSON string
-    file_content = json.dumps(contentBody)
-    
-    # Write the file to S3
-    file_key = cet_time.strftime(f"%Y/%m/%d/%H/%M/{record_type}/{saml_request_id}.json")
     try:
+    
+        for record in event:
         
-      s3.Bucket(bucket_name).put_object(Key=file_key, Body=file_content)
+            saml_request_id = record['dynamodb']['NewImage']['samlRequestID']['S']
+            record_type = record['dynamodb']['NewImage']['recordType']['S']
+            creation_time = record['dynamodb']['NewImage']['creationTime']['N']
+            contentBody = record['dynamodb']['NewImage']
+            cet_time = convert_to_cet(int(creation_time))
         
-      return {
-          'statusCode': 200,
-          'body': json.dumps(f'Successfully wrote file {file_key} to bucket {bucket_name}')
-      }
+            if record_type == "SAML" :
+                contentBody = decode_base64_content(contentBody)
+            
+            # Write the file to S3
+            file_key = cet_time.strftime(f"%Y/%m/%d/%H/%M/{record_type}/{saml_request_id}.json")
+                
+            s3.Bucket(bucket_name).put_object(Key=file_key, Body=json.dumps(contentBody))
+        
     except Exception as e:
         logger.error(e)
         return {
