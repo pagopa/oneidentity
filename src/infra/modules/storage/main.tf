80bb6ff7f7cc6e4997cc8935526b19d0ecc16ddb
Walter Traspadini <walter.traspadini@pagopa.it>
2024-07-19 16:07:38 +0200

    feat: First draft athena query assertions (#198)
    * first draft athena query
    
    * update athena create table
    
    * assertion bucket as parameter in athema table crate

--- src/infra/modules/storage/main.tf
+++ src/infra/modules/storage/main.tf
@@ -5,7 +5,9 @@ resource "random_integer" "assertion_bucket_suffix" {
 
 locals {
   bucket_name = format("%s-%s", var.assertion_bucket.name_prefix,
-  random_integer.assertion_bucket_suffix.result)
+    random_integer.assertion_bucket_suffix.result
+  )
+  athena_outputs = format("query-%s", local.bucket_name)
 
 }
 
@@ -47,11 +49,6 @@ module "s3_assertions_bucket" {
     mfa_delete = var.assertion_bucket.mfa_delete
   }
 
-  logging = {
-    target_bucket = local.bucket_name
-    target_prefix = "logs/"
-  }
-
   lifecycle_rule = [
     {
       enabled = true
@@ -75,4 +72,95 @@ module "s3_assertions_bucket" {
   tags = {
     Name = local.bucket_name
   }
+}
+
+## Athena ##
+
+module "s3_athena_output_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.1"
+
+  bucket = local.athena_outputs
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  server_side_encryption_configuration = {
+    rule = {
+      bucket_key_enabled = true
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = module.kms_assertions_bucket.aliases["assertions/S3"].arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  tags = {
+    Name = local.bucket_name
+  }
+}
+
+resource "aws_athena_workgroup" "assertions_workgroup" {
+  name = "assertions_workgroup"
+
+  configuration {
+    result_configuration {
+      output_location = "s3://${local.athena_outputs}/output/"
+    }
+  }
+}
+
+# Create Athena database
+resource "aws_athena_database" "assertions" {
+  name   = "assertions"
+  bucket = module.s3_athena_output_bucket.s3_bucket_id
+}
+
+# Create Athena table
+resource "aws_athena_named_query" "create_assertions_table" {
+  name     = "create_assertions_table"
+  database = aws_athena_database.assertions.name
+  workgroup = aws_athena_workgroup.assertions_workgroup.id
+  query    = <<EOF
+CREATE EXTERNAL TABLE `${module.s3_assertions_bucket.s3_bucket_id}`(
+  `samlrequestid` string COMMENT 'from deserializer', 
+  `recordtype` string COMMENT 'from deserializer', 
+  `creationtime` string COMMENT 'from deserializer', 
+  `clientid` string COMMENT 'from deserializer', 
+  `idp` string COMMENT 'from deserializer', 
+  `nonce` string COMMENT 'from deserializer', 
+  `redirecturi` string COMMENT 'from deserializer', 
+  `responsetype` string COMMENT 'from deserializer', 
+  `samlrequest` string COMMENT 'from deserializer', 
+  `samlresponse` string COMMENT 'from deserializer', 
+  `scope` string COMMENT 'from deserializer', 
+  `state` string COMMENT 'from deserializer', 
+  `ttl` string COMMENT 'from deserializer', 
+  `code` string COMMENT 'from deserializer', 
+  `idtoken` string COMMENT 'from deserializer', 
+  `eventname` string COMMENT 'from deserializer')
+PARTITIONED BY ( 
+  `partition_0` string, 
+  `partition_1` string, 
+  `partition_2` string, 
+  `partition_3` string, 
+  `partition_4` string, 
+  `partition_5` string)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'paths'='SAMLRequest,SAMLResponse,clientId,code,creationTime,eventName,idToken,idp,nonce,recordType,redirectUri,responseType,samlRequestID,scope,state,ttl') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://${module.s3_assertions_bucket.s3_bucket_id}/'
+TBLPROPERTIES (
+  'classification'='json', 
+  'compressionType'='none', 
+  'partition_filtering.enabled'='true',
+  'typeOfData'='file')
+EOF
 }
