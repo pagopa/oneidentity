1ad9cb3c2534abcda86678a25a06ee8676472e33
uolter <walter.traspadini@pagopa.it>
2024-08-13 10:38:07 +0200

    uat client registratin lambda vpc integration

--- src/infra/uat/main.tf
+++ src/infra/uat/main.tf
@@ -155,7 +155,9 @@ module "backend" {
     filename                          = "${path.module}/../hello-java/build/libs/hello-java-1.0-SNAPSHOT.jar"
     table_client_registrations_arn    = module.database.table_client_registrations_arn
     cloudwatch_logs_retention_in_days = var.lambda_cloudwatch_logs_retention_in_days
-
+    vpc_id                            = module.network.vpc_id
+    vpc_subnet_ids                    = module.network.intra_subnets_ids
+    vpc_endpoint_dynamodb_prefix_id   = module.network.vpc_endpoints["dynamodb"]["prefix_list_id"]
   }
 
   metadata_lambda = {
