34987581a71c287cb52df800e09d496762f2f1a5
uolter <walter.traspadini@pagopa.it>
2024-08-22 10:40:46 +0200

    fix var name

--- src/infra/modules/monitoring/main.tf
+++ src/infra/modules/monitoring/main.tf
@@ -3,12 +3,12 @@ resource "aws_cloudwatch_dashboard" "main" {
 
   dashboard_body = templatefile("../dashboards/main.tpl.json",
     {
-      aws_region           = var.aws_region
-      api_name             = var.api_name
-      sessions_table       = var.sessions_table
-      client_registrations = var.client_registrations_table
-      ecs                  = var.ecs
-      nlb                  = var.nlb
+      aws_region                 = var.aws_region
+      api_name                   = var.api_name
+      sessions_table             = var.sessions_table
+      client_registrations_table = var.client_registrations_table
+      ecs                        = var.ecs
+      nlb                        = var.nlb
 
     }
   )
