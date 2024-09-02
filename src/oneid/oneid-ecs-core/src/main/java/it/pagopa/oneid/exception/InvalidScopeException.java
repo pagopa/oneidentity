a8a3e73733b79b50bf44c17b2caf7051dfd156f7
Giuseppe Montesano <104127973+GiuMontesano@users.noreply.github.com>
2024-09-02 12:39:38 +0200

    Test: ecs core exception mapper (#277)
    * Feat: update some exceptions related to OAuth2 errors
    
    * test: add test for ExceptionMapper

--- src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/exception/InvalidScopeException.java
+++ src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/exception/InvalidScopeException.java
@@ -1,11 +1,14 @@
 package it.pagopa.oneid.exception;
 
+import com.nimbusds.oauth2.sdk.OAuth2Error;
 import it.pagopa.oneid.common.model.exception.AuthorizationErrorException;
 import it.pagopa.oneid.common.model.exception.enums.ErrorCode;
 
 public class InvalidScopeException extends AuthorizationErrorException {
 
   public InvalidScopeException(String callbackUri, String state) {
-    super(String.valueOf(ErrorCode.INVALID_SCOPE_ERROR), callbackUri, state);
+    super(String.valueOf(ErrorCode.INVALID_SCOPE_ERROR), callbackUri,
+        ErrorCode.INVALID_SCOPE_ERROR.getErrorMessage(), OAuth2Error.INVALID_SCOPE_CODE,
+        state);
   }
 }
