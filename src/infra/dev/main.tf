495188cc8db2e6eab072ad34ccb25b8a39e7207a
uolter <walter.traspadini@pagopa.it>
2024-09-04 10:50:17 +0200

    missed new prefix in iam module

--- src/infra/dev/main.tf
+++ src/infra/dev/main.tf
@@ -1,8 +1,8 @@
 data "aws_caller_identity" "current" {}
 
 module "iam" {
-  source = "../modules/iam"
-
+  source            = "../modules/iam"
+  prefix            = local.project
   github_repository = "pagopa/oneidentity"
 }
 
