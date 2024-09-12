1c59665f7fdc65f4ca43286d6f9009d56a20f91c
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2024-09-12 09:58:07 +0200

    feat: added lambda alarms (#305)
    * feat: added lambda alarms
    
    * terraform-docs: automated action
    
    * refactory lambda_alarms variable
    
    * terraform-docs: automated action
    
    * removed prefix lambda in function name
    
    * fix function name assertion
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: uolter <walter.traspadini@pagopa.it>

--- src/infra/prod/eu-south-1/variables.tf
+++ src/infra/prod/eu-south-1/variables.tf
@@ -337,28 +337,23 @@ variable "ecs_alarms" {
 }
 
 variable "lambda_alarms" {
-  type = object({
-    metric_name         = string
-    namespace           = string
-    threshold           = optional(number)
-    evaluation_periods  = optional(number)
-    period              = optional(number)
-    statistic           = optional(string)
-    comparison_operator = optional(string)
-    sns_topic_alarm_arn = optional(list(string))
-    treat_missing_data  = optional(string)
-  })
+  type = map(object({
+    metric_name         = optional(string, "Errors")
+    namespace           = optional(string, "AWS/Lambda")
+    threshold           = optional(number, 1)
+    evaluation_periods  = optional(number, 1)
+    period              = optional(number, 300)
+    statistic           = optional(string, "Sum")
+    comparison_operator = optional(string, "GreaterThanOrEqualToThreshold")
+    treat_missing_data  = optional(string, "notBreaching")
+  }))
 
   default = {
-
-    metric_name         = "Errors"
-    namespace           = "AWS/Lambda"
-    threshold           = 1
-    evaluation_periods  = 1
-    comparison_operator = "GreaterThanOrEqualToThreshold"
-    period              = 300
-    statistic           = "Sum"
-    treat_missing_data  = "notBreaching"
+    "oneid-es-1-p-assertion" = {
+    },
+    "oneid-es-1-p-metadata" = {},
+    "oneid-es-1-p-client-registration" = {
+    },
   }
 }
 
