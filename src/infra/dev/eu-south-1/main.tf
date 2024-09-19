9523514a67b190c9e07715adc2bfbac484727fef
Walter Traspadini <walter.traspadini@pagopa.it>
2024-09-19 18:23:06 +0200

    update open api response login alert message json (#328)

--- src/infra/dev/eu-south-1/main.tf
+++ src/infra/dev/eu-south-1/main.tf
@@ -237,21 +237,21 @@ module "backend" {
     name                              = format("%s-update-idp-metadata", local.project)
     filename                          = "${path.module}/../../hello-java/build/libs/hello-java-1.0-SNAPSHOT.jar"
     cloudwatch_logs_retention_in_days = var.lambda_cloudwatch_logs_retention_in_days
-    environment_variables             = {
-      IDP_METADATA_BUCKET_NAME  = module.storage.s3_idp_metadata_bucket_name 
-      IDP_TABLE_NAME            = module.database.table_idp_metadata_name
-      IDP_G_IDX                 = module.database.table_idp_metadata_idx_name
-    }  
-    s3_idp_metadata_bucket_arn        = module.storage.idp_metadata_bucket_arn
-    s3_idp_metadata_bucket_id         = module.storage.s3_idp_metadata_bucket_name
-    vpc_id                            = module.network.vpc_id
-    vpc_subnet_ids                    = module.network.intra_subnets_ids
-    vpc_s3_prefix_id                  = module.network.vpc_endpoints["s3"]["prefix_list_id"]
+    environment_variables = {
+      IDP_METADATA_BUCKET_NAME = module.storage.s3_idp_metadata_bucket_name
+      IDP_TABLE_NAME           = module.database.table_idp_metadata_name
+      IDP_G_IDX                = module.database.table_idp_metadata_idx_name
+    }
+    s3_idp_metadata_bucket_arn = module.storage.idp_metadata_bucket_arn
+    s3_idp_metadata_bucket_id  = module.storage.s3_idp_metadata_bucket_name
+    vpc_id                     = module.network.vpc_id
+    vpc_subnet_ids             = module.network.intra_subnets_ids
+    vpc_s3_prefix_id           = module.network.vpc_endpoints["s3"]["prefix_list_id"]
   }
 
   dynamodb_table_idpMetadata = {
     gsi_pointer_arn = module.database.table_idpMetadata_gsi_pointer_arn
-    table_arn = module.database.table_idp_metadata_arn
+    table_arn       = module.database.table_idp_metadata_arn
   }
 }
 
