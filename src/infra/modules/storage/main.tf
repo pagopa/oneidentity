4af3a016bf4708097337ec63a889c0899154f895
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2024-09-25 17:51:04 +0200

    feat: github permission to upload idp (#363)
    * feat: github permission to upload idp
    
    * terraform-docs: automated action
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: uolter <walter.traspadini@pagopa.it>

--- src/infra/modules/storage/main.tf
+++ src/infra/modules/storage/main.tf
@@ -94,3 +94,59 @@ resource "aws_iam_policy" "github_s3_policy" {
     ]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "upload_idp" {
+  role       = aws_iam_role.upload_idp_role.name
+  policy_arn = aws_iam_policy.upload_idp_policy.arn
+}
+
+resource "aws_iam_role" "upload_idp_role" {
+  name        = "${var.role_prefix}-upload-idp"
+  description = "Role to assume to copy to S3."
+
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          "Federated" : "arn:aws:iam::${var.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" : "repo:${var.github_repository}:*"
+          },
+          "ForAllValues:StringEquals" = {
+            "token.actions.githubusercontent.com:iss" : "https://token.actions.githubusercontent.com",
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "upload_idp_policy" {
+  name        = "${var.role_prefix}-upload-idp"
+  description = "Policy to deploy to S3"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = [
+          module.s3_idp_metadata_bucket.s3_bucket_arn,
+          "${module.s3_idp_metadata_bucket.s3_bucket_arn}/*"
+        ]
+      }
+    ]
+  })
+}
