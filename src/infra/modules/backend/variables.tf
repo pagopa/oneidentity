a5b6f58bc9537bdb01d50e8dbca1a923ac2c64e6
BenitoVisone <visonebenito@gmail.com>
2024-09-30 12:00:07 +0200

    feat: setup policy for GetParameter SSM action in ecs-core

--- src/infra/modules/backend/variables.tf
+++ src/infra/modules/backend/variables.tf
@@ -15,8 +15,8 @@ variable "role_prefix" {
 
 variable "ecr_registers" {
   type = list(object({
-    name                            = string
-    number_of_images_to_keep        = number
+    name                     = string
+    number_of_images_to_keep = number
     repository_image_tag_mutability = optional(string, "IMMUTABLE")
   }))
   description = "ECR image repositories"
@@ -44,9 +44,9 @@ variable "fargate_capacity_providers" {
 
 variable "service_core" {
   type = object({
-    service_name           = string
-    cpu                    = number
-    memory                 = number
+    service_name = string
+    cpu          = number
+    memory       = number
     enable_execute_command = optional(bool, true)
     container = object({
       name                = string
@@ -92,7 +92,6 @@ variable "dynamodb_table_idpMetadata" {
   description = "Dynamodb table idpMetadata anrs"
 }
 
-
 variable "table_client_registrations_arn" {
   type        = string
   description = "Dynamodb table client registrations arn."
@@ -111,7 +110,7 @@ variable "client_registration_lambda" {
     cloudwatch_logs_retention_in_days = number
     vpc_id                            = string
     vpc_endpoint_dynamodb_prefix_id   = string
-    vpc_subnet_ids                    = list(string)
+    vpc_subnet_ids = list(string)
 
   })
 
@@ -122,9 +121,9 @@ variable "metadata_lambda" {
     name                              = string
     filename                          = string
     table_client_registrations_arn    = string
-    environment_variables             = map(string)
+    environment_variables = map(string)
     vpc_id                            = string
-    vpc_subnet_ids                    = list(string)
+    vpc_subnet_ids = list(string)
     vpc_endpoint_dynamodb_prefix_id   = string
     cloudwatch_logs_retention_in_days = number
   })
@@ -143,7 +142,7 @@ variable "vpc_id" {
 }
 
 variable "private_subnets" {
-  type        = list(string)
+  type = list(string)
   description = "Private subnets ids."
 }
 
@@ -164,10 +163,10 @@ variable "assertion_lambda" {
     filename                          = string
     s3_assertion_bucket_arn           = string
     kms_assertion_key_arn             = string
-    environment_variables             = map(string)
+    environment_variables = map(string)
     cloudwatch_logs_retention_in_days = number
     vpc_s3_prefix_id                  = string
-    vpc_subnet_ids                    = list(string)
+    vpc_subnet_ids = list(string)
     vpc_id                            = string
   })
 }
@@ -176,11 +175,11 @@ variable "idp_metadata_lambda" {
   type = object({
     name                              = string
     filename                          = string
-    environment_variables             = map(string)
+    environment_variables = map(string)
     s3_idp_metadata_bucket_arn        = string
     s3_idp_metadata_bucket_id         = string
     vpc_id                            = string
-    vpc_subnet_ids                    = list(string)
+    vpc_subnet_ids = list(string)
     vpc_s3_prefix_id                  = string
     cloudwatch_logs_retention_in_days = number
   })
@@ -191,9 +190,9 @@ variable "is_gh_integration_lambda" {
   type = object({
     name                              = string
     filename                          = string
-    sns_topic_arn                     = optional(string, null)
+    sns_topic_arn = optional(string, null)
     cloudwatch_logs_retention_in_days = string
-    ssm_parameter_name                = optional(string, "GH_PERSONAL_ACCESS_TOKEN")
+    ssm_parameter_name = optional(string, "GH_PERSONAL_ACCESS_TOKEN")
   })
 
 }
@@ -211,7 +210,6 @@ variable "eventbridge_pipe_sessions" {
 variable "sns_topic_arn" {
   type = string
 }
-
 
 variable "ecs_alarms" {
   type = map(object({
