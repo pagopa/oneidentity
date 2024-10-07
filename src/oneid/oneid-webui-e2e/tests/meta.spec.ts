d63b767b3a2d87b67d9bc787aa3f31c847be4095
Michele Moio <michelemoiodev@gmail.com>
2024-10-07 17:48:23 +0200

    test: [OI-211] playwright and basic e2e test

---
+++ src/oneid/oneid-webui-e2e/tests/meta.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/login');
+
+  // Expect title "to contain" substring
+  await expect(page).toHaveTitle(/PagoPA One Identity – Login/);
+});
