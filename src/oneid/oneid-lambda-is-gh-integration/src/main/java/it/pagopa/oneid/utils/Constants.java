3b99a9a057bf4296e55d703a61a9fe5c6afd3eed
giuseppe-gangemi <giuseppe.gangemi@pagopa.it>
2024-10-10 14:50:29 +0200

    chore: move metadata directory

--- src/oneid/oneid-lambda-is-gh-integration/src/main/java/it/pagopa/oneid/utils/Constants.java
+++ src/oneid/oneid-lambda-is-gh-integration/src/main/java/it/pagopa/oneid/utils/Constants.java
@@ -4,6 +4,6 @@ public abstract class Constants {
 
   public static String GH_PERSONAL_ACCESS_TOKEN = "GH_PERSONAL_ACCESS_TOKEN";
   public static String REPOSITORY_NAME = "pagopa/oneidentity";
-  public static String METADATA_BASE_PATH = "src/oneid/oneid-ecs-core/metadata/";
+  public static String METADATA_BASE_PATH = "src/oneid/metadata/";
   public static String BRANCH_BASE_NAME = "idp-metadata-update-";
 }
