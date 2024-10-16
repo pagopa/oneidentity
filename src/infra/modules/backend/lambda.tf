60e19dc2897678c11c036f1898226c76118b2299
uolter <walter.traspadini@pagopa.it>
2024-10-16 14:57:29 +0200

    refactoring nsg rule lambda metadata

--- src/infra/modules/backend/lambda.tf
+++ src/infra/modules/backend/lambda.tf
@@ -72,7 +72,9 @@ module "security_group_lambda_client_registration" {
   egress_ipv6_cidr_blocks = []
 
   # Prefix list ids to use in all egress rules in this module
-  egress_prefix_list_ids = [var.metadata_lambda.vpc_endpoint_dynamodb_prefix_id]
+  egress_prefix_list_ids = [
+    var.client_registration_lambda.vpc_endpoint_dynamodb_prefix_id,
+  ]
 
   egress_rules = ["https-443-tcp"]
 }
@@ -231,7 +233,10 @@ module "security_group_lambda_idp_metadata" {
   egress_ipv6_cidr_blocks = []
 
   # Prefix list ids to use in all egress rules in this module
-  egress_prefix_list_ids = [var.metadata_lambda.vpc_endpoint_dynamodb_prefix_id, var.idp_metadata_lambda.vpc_s3_prefix_id, ]
+  egress_prefix_list_ids = [
+    var.metadata_lambda.vpc_endpoint_dynamodb_prefix_id,
+    var.idp_metadata_lambda.vpc_s3_prefix_id,
+  ]
 
   egress_rules = ["https-443-tcp"]
 }
