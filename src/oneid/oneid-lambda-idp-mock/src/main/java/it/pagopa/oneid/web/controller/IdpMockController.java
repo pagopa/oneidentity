85059eb4e7576ea234fa7041b60140ef14143433
BenitoVisone <visonebenito@gmail.com>
2024-10-22 17:06:27 +0200

    chore: temporary hardcode saml acs

--- src/oneid/oneid-lambda-idp-mock/src/main/java/it/pagopa/oneid/web/controller/IdpMockController.java
+++ src/oneid/oneid-lambda-idp-mock/src/main/java/it/pagopa/oneid/web/controller/IdpMockController.java
@@ -38,8 +38,9 @@ public class IdpMockController {
 
     String xmlResponseBase64 = Base64.getEncoder().encodeToString(xmlResponse.getBytes());
 
+    // TODO remove hardcoded acs
     String redirectAutoSubmitPOSTForm =
-        "<form method='post' action=" + inputAuthnRequest.getAssertionConsumerServiceURL()
+        "<form method='post' action=https://dev.oneid.pagopa.it/saml/acs"
             + " id='SAMLResponseForm'>" +
             "<input type='hidden' name='SAMLResponse' value=" + xmlResponseBase64 + " />" +
             "<input type='hidden' name='RelayState' value=" + relayState + " />" +
