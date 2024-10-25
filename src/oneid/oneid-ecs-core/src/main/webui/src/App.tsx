a32c63e46f38cb5c2ad95abb778a405296755ee3
Alessandro Sebastiani <sebbalex@gmail.com>
2024-10-25 16:24:35 +0200

    fix: remove frontend useless code

--- src/oneid/oneid-ecs-core/src/main/webui/src/App.tsx
+++ src/oneid/oneid-ecs-core/src/main/webui/src/App.tsx
@@ -3,21 +3,10 @@ import { ROUTE_LOGIN, ROUTE_LOGIN_ERROR, ROUTE_LOGOUT } from './utils/constants'
 import { redirectToLogin } from './utils/utils';
 import Logout from './pages/logout/Logout';
 import { LoginError } from './pages/loginError/LoginError';
-import { trackEvent } from './services/analyticsService';
 
 const onLogout = () => <Logout />;
 const onLoginError = () => <LoginError />;
-
-/** login request operations */
-const onLoginRequest = () => {
-  handleLoginRequestOnSuccessRequest();
-  return <Login />;
-};
-
-const handleLoginRequestOnSuccessRequest = () => {
-  const onSuccess: string | null = new URLSearchParams(window.location.search).get('onSuccess');
-  trackEvent('LOGIN_INTENT', { target: onSuccess ?? 'dashboard' });
-};
+const onLoginRequest = () => <Login />;
 
 function App() {
   if (window.location.pathname === ROUTE_LOGOUT) {
