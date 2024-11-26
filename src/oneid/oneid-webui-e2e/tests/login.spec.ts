5547c21bba04289ae68a1242b5ee43b97cc316d2
Michele Moio <michelemoiodev@gmail.com>
2024-11-26 13:01:57 +0100

    fix: [OI-275] slows login test

--- src/oneid/oneid-webui-e2e/tests/login.spec.ts
+++ src/oneid/oneid-webui-e2e/tests/login.spec.ts
@@ -2,11 +2,13 @@ import { expect, test } from '@playwright/test';
 import { TEST_IDP_ID, TEST_LOGIN_URL } from './utils/constants';
 
 test('auth is successful', async ({ page }) => {
+  test.slow();
   const nonce = process.env.NONCE as string;
   await page.goto(TEST_LOGIN_URL);
 
   await page.locator('#spidButton').click();
   await page.locator(`[id="${TEST_IDP_ID}"]`).click();
   await page.waitForURL('**/client/cb?*');
+  expect(page.getByText(nonce)).toBeTruthy();
   expect(page.getByText(nonce)).toBeVisible();
 });
