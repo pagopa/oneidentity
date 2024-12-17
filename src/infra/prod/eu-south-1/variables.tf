0fe0b123455e8426055ae6447390620bc1ef8475
uolter <walter.traspadini@pagopa.it>
2024-12-17 16:13:28 +0100

    update sns topic is arn

--- src/infra/prod/eu-south-1/variables.tf
+++ src/infra/prod/eu-south-1/variables.tf
@@ -395,7 +395,7 @@ variable "alarm_subscribers" {
 
 variable "is_gh_sns_arn" {
   type    = string
-  default = "arn:aws:sns:eu-south-1:001102221608:history"
+  default = "arn:aws:sns:eu-south-1:116453376486:history"
 }
 
 variable "ssm_cert_key" {
