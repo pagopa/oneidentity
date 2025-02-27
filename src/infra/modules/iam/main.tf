cb8f1428b36d51c90bbaaff9b99e0246afcab2ad
uolter <walter.traspadini@pagopa.it>
2025-02-27 20:24:36 +0100

    fix trusted policy

--- src/infra/modules/iam/main.tf
+++ src/infra/modules/iam/main.tf
@@ -38,7 +38,7 @@ resource "aws_iam_role" "githubiac" {
         },
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
-          StringLike = {
+          StringEquals = {
             "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com",
             "token.actions.githubusercontent.com:sub" : "repo:${var.github_repository}:ref:refs/heads/main"
           }
