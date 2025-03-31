f245edd6185115531fd856af0f8e67e7bb1d466b
Alessandro Sebastiani <sebbalex@users.noreply.github.com>
2025-03-31 09:54:29 +0200

    fix: e2e test due to meta title change

--- src/oneid/oneid-webui-e2e/tests/meta.spec.ts
+++ src/oneid/oneid-webui-e2e/tests/meta.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/login');
   // Expect title "to contain" substring
-  await expect(page).toHaveTitle(/PagoPA One Identity – Login/);
+  await expect(page).toHaveTitle(/Accedi · PagoPA S.p.A./);
 });
