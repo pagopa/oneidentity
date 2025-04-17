9ff43a05520dec92396a4b2d56dd4dab2093aec3
BenitoVisone <visonebenito@gmail.com>
2025-04-17 14:16:56 +0200

    feat: add oneid-lambda-invalidate-cache code

---
+++ src/oneid/oneid-lambda-invalidate-cache/index.py
@@ -0,0 +1,30 @@
+"""
+This Lambda function flushes the cache of a specific stage in an API Gateway REST API.
+"""
+
+import boto3
+import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+api_gateway_client = boto3.client('apigateway')
+
+REST_API_ID = os.environ.get("REST_API_ID")
+STAGE_NAME = os.environ.get("STAGE_NAME")
+
+def lambda_handler(event, context):
+  """
+  Lambda function handler to flush the cache of a specific stage in an API Gateway REST API.
+  """
+  try:
+        api_gateway_client.flush_stage_cache(
+            restApiId=REST_API_ID,
+            stageName=STAGE_NAME,
+        )
+        logger.info("Stage cache flushed successfully.")
+        return True
+    except Exception as ex:
+        logger.error("Error during flush stage cache operation: %s", repr(ex))
+        return False
