5a3a86df3dcf61a81288abab0ebb9c5f47550ea1
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2025-04-29 14:46:42 +0200

    remove vpc reference from envs

--- src/infra/modules/backend/variables.tf
+++ src/infra/modules/backend/variables.tf
@@ -303,10 +303,10 @@ variable "invalidate_cache_lambda" {
     filename                          = string
     cloudwatch_logs_retention_in_days = string
     environment_variables             = map(string)
-    vpc_endpoint_apigw_prefix_id      = string
+   # vpc_endpoint_apigw_prefix_id      = string
     vpc_endpoint_dynamodb_prefix_id   = string
-    vpc_subnet_ids                    = list(string)
-    vpc_id                            = string
+   # vpc_subnet_ids                    = list(string)
+   # vpc_id                            = string
     rest_api_execution_arn            = string
     rest_api_arn                      = string
   })
