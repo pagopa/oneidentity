32a94f587cad411d3000af72db86b0366fe86782
BenitoVisone <visonebenito@gmail.com>
2025-04-30 12:30:22 +0200

    feat: implement update optional attributes route

--- src/oneid/oneid-lambda-client-manager/index.py
+++ src/oneid/oneid-lambda-client-manager/index.py
@@ -1,4 +1,4 @@
-"""Lambda function which servers as BE for Client Portal"""
+"""Lambda function which serves as BE for Client Portal"""
 
 import boto3
 import logging
@@ -7,6 +7,7 @@ from typing import Optional
 from aws_lambda_powertools import Tracer
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from localized_content_map import LocalizedContentMap
 
 
 # ENVIRONMENT VARIABLES
@@ -14,6 +15,7 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 # AWS_REGION
 # USER_POOL_ID
 # LOG_LEVEL
+# CLIENT_REGISTRATIONS_TABLE
 
 # Get tracer
 tracer = Tracer()
@@ -27,18 +29,33 @@ app = APIGatewayRestResolver()
 
 aws_region = os.getenv("AWS_REGION")
 
-def get_cognito_client(region: str) -> Optional["boto3.client.cognito-identity"]:
+
+def check_client_id_exists(client_id: str) -> bool:
+    """
+    Check if client_id exists in ClientRegistrations table
+    """
+    try:
+        response = dynamodb_client.get_item(
+            TableName=os.getenv("CLIENT_REGISTRATIONS_TABLE"),
+            Key={"client_id": {"S": client_id}},
+        )
+        logger.debug("[check_client_id_exists]: %s", response)
+        return "Item" in response
+    except Exception as ex:
+        logger.error("[check_client_id_exists]: %s", repr(ex))
+        return False
+
+def get_cognito_client(region: str) -> Optional["boto3.client.cognito-idp"]:
     """
     Retrieve cognito client
     """
     try:
-        client = boto3.client("cognito-identity", region_name=region)
+        client = boto3.client("cognito-idp", region_name=region)
         logger.debug("[get_cognito_client]: client up")
         return client
     except Exception as ex:
         logger.error("[get_cognito_client]: %s", repr(ex))
         return None
-
 
 def get_dynamodb_client(region: str) -> Optional["boto3.client.DynamoDB"]:
     """
@@ -64,6 +81,7 @@ def update_user_attributes_with_client_id():
     """
     Inserts 'client_id' inside Cognito User Attributes
     """
+    logger.info("/admin/client-manager/user-attributes route invoked")
     try:
         # Parse the JSON body of the request
         body = app.current_event.json_body
@@ -78,25 +96,24 @@ def update_user_attributes_with_client_id():
         user_id = body.get("user_id")
 
         if not client_id or not user_id:
-          return {"message": "client_id and user_id are required"}, 400
+            return {"message": "client_id and user_id are required"}, 400
         
+        # Check if client_id exists in ClientRegistrations table
+        if not check_client_id_exists(client_id):
+            return {"message": "client_id not found"}, 404
+
         # Update the user attributes in Cognito inserting the client_id
         response = cognito_client.admin_update_user_attributes(
             UserPoolId=os.getenv("USER_POOL_ID"),
             Username=user_id,
-            UserAttributes=[
-                {
-                    "Name": "custom:client_id",
-                    "Value": client_id
-                }
-            ]
+            UserAttributes=[{"Name": "custom:client_id", "Value": client_id}],
         )
         logger.debug("[update_user_attributes_with_client_id]: %s", response)
         # Check if the response indicates success
         if response.get("ResponseMetadata", {}).get("HTTPStatusCode") != 200:
             logger.error("[update_user_attributes_with_client_id]: %s", response)
             return {"message": "Failed to update user attributes"}, 500
-        
+
         # Return success response
         return {"message": "Optional attributes updated successfully"}, 200
 
@@ -105,12 +122,56 @@ def update_user_attributes_with_client_id():
         return {"message": "Internal server error"}, 500
     return None
 
-# TODO implement function
+
 @app.put("/admin/client-manager/client-additional/<client_id>")
 def update_optional_attributes(client_id):
     """
     Updates optional fields on 'ClientRegistrations' Table
     """
+    logger.info("/admin/client-manager/client-additional route invoked")
+    try:
+        # Parse the JSON body of the request
+        body = app.current_event.json_body
+
+        # Validate the body (optional)
+        if not body:
+            return {"message": "Request body is required"}, 400
+        
+        # Check if client_id exists in ClientRegistrations table
+        if not check_client_id_exists(client_id):
+            return {"message": "client_id not found"}, 404
+
+        # TODO: check if client_id is associated with the Cognito user_id which executed the request
+
+        # Extract optional attributes from the request body
+        a11y_uri = body.get("a11y_uri")
+        back_button_enabled = body.get("back_button_enabled")
+        localized_content = body.get("localized_content")
+
+        localized_content_map_object = LocalizedContentMap.from_json(localized_content)
+        localized_content_map_object_value = localized_content_map_object.to_dynamodb()
+
+        # Update the optional attributes in DynamoDB in ClientRegistrations table
+        response = dynamodb_client.update_item(
+            TableName=os.getenv("CLIENT_REGISTRATIONS_TABLE"),
+            Key={"client_id": {"S": client_id}},
+            UpdateExpression="SET a11y_uri = :a11y_uri, back_button_enabled = :back_button_enabled, localized_content = :localized_content",
+            ExpressionAttributeValues={
+                ":a11y_uri": {"S": a11y_uri},
+                ":back_button_enabled": {"BOOL": back_button_enabled},
+                ":localized_content": localized_content_map_object_value,
+            },
+        )
+        logger.debug("[update_optional_attributes]: %s", response)
+
+        # Check if the response indicates success
+        if response.get("ResponseMetadata", {}).get("HTTPStatusCode") != 200:
+            logger.error("[update_optional_attributes]: %s", response)
+            return {"message": "Failed to update optional attributes"}, 500
+
+    except Exception as e:
+        logger.error("Error updating optional attributes: %s", repr(e))
+        return {"message": "Internal server error"}, 500
     return None
 
 
