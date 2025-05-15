1617556a8fa05f0d4f3ab19c1672fc3c4d32eb9c
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2025-05-15 16:53:12 +0200

    fix: uat env

--- src/infra/uat/eu-south-1/main.tf
+++ src/infra/uat/eu-south-1/main.tf
@@ -37,12 +37,15 @@ module "frontend" {
 
   ## DNS
   domain_name     = module.r53_zones.dns_zone_name
+  domain_admin_name = module.r53_zones.dns_zone_name
   r53_dns_zone_id = module.r53_zones.dns_zone_id
   role_prefix     = local.project
 
   ## API Gateway ##
   rest_api_name         = format("%s-restapi", local.project)
+  rest_api_admin_name   = format("%s-restapi-admin", local.project)
   openapi_template_file = "../../api/oi.tpl.json"
+  openapi_admin_template_file = "../../api/oi-admin.tpl.json"
 
   dns_record_ttl = var.dns_record_ttl
 
@@ -56,6 +59,12 @@ module "frontend" {
     api_key_name         = "client-registration"
   }
 
+  api_gateway_admin_plan = {
+    name                 = format("%s-restapi-admin_plan", local.project)
+    throttle_burst_limit = var.rest_api_throttle_settings.burst_limit
+    throttle_rate_limit  = var.rest_api_throttle_settings.rate_limit
+  }
+  client_manager_lambda_arn      = ""
   client_registration_lambda_arn = module.backend.client_registration_lambda_arn
   retrieve_status_lambda_arn     = module.backend.retrieve_status_lambda_arn
   aws_region                     = var.aws_region
