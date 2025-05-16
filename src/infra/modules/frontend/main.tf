8a57224e5856d88aaca3d5bfa72b01387e81c294
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2025-05-16 12:03:33 +0200

    fix ref count

--- src/infra/modules/frontend/main.tf
+++ src/infra/modules/frontend/main.tf
@@ -10,8 +10,8 @@ module "records" {
       name = ""
       type = "A"
       alias = {
-        name                   = module.rest_api[0].regional_domain_name
-        zone_id                = module.rest_api[0].regional_zone_id
+        name                   = module.rest_api.regional_domain_name
+        zone_id                = module.rest_api.regional_zone_id
         evaluate_target_health = true
         ttl                    = var.dns_record_ttl
       }
