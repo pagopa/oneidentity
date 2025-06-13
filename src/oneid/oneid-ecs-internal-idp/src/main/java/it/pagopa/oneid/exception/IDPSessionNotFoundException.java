81b435f20241f0035117431364dba678de92fe1e
BenitoVisone <visonebenito@gmail.com>
2025-06-13 12:39:13 +0200

    feat: add custom exceptions

---
+++ src/oneid/oneid-ecs-internal-idp/src/main/java/it/pagopa/oneid/exception/IDPSessionNotFoundException.java
@@ -0,0 +1,8 @@
+package it.pagopa.oneid.exception;
+
+public class IDPSessionNotFoundException extends RuntimeException {
+
+  public IDPSessionNotFoundException(String message) {
+    super(message);
+  }
+}
