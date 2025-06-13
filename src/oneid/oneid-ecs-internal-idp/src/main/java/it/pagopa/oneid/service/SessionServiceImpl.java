e623b1d423d6ab275fa386de686e4ee5b2ad2907
giuseppe-gangemi <giuseppe.gangemi@pagopa.it>
2025-06-13 11:56:25 +0200

    feat: add initial test classes for SessionConnector

--- src/oneid/oneid-ecs-internal-idp/src/main/java/it/pagopa/oneid/service/SessionServiceImpl.java
+++ src/oneid/oneid-ecs-internal-idp/src/main/java/it/pagopa/oneid/service/SessionServiceImpl.java
@@ -2,8 +2,10 @@ package it.pagopa.oneid.service;
 
 import it.pagopa.oneid.connector.SessionConnectorImpl;
 import it.pagopa.oneid.model.IDPSession;
+import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 
+@Dependent
 public class SessionServiceImpl implements SessionService {
 
   @Inject
