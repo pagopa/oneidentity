86287ac0be9f86e9dda17f7944281a4d4052e813
BenitoVisone <visonebenito@gmail.com>
2025-06-13 12:44:52 +0200

    feat: add updateIDPSession and getIDPSessionByAuthnRequestIDAndUsername in connector

--- src/oneid/oneid-ecs-internal-idp/src/main/java/it/pagopa/oneid/connector/SessionConnector.java
+++ src/oneid/oneid-ecs-internal-idp/src/main/java/it/pagopa/oneid/connector/SessionConnector.java
@@ -2,9 +2,16 @@ package it.pagopa.oneid.connector;
 
 import it.pagopa.oneid.common.model.exception.OneIdentityException;
 import it.pagopa.oneid.model.IDPSession;
+import it.pagopa.oneid.model.enums.IDPSessionStatus;
+import java.util.Optional;
 
 public interface SessionConnector {
 
   void saveIDPSessionIfNotExists(IDPSession idpSession) throws OneIdentityException;
+
+  Optional<IDPSession> getIDPSessionByAuthnRequestIdAndUsername(String authnRequestId,
+      String username);
+
+  void updateIDPSession(IDPSession idpSession, Optional<IDPSessionStatus> previousStatus);
 
 }
