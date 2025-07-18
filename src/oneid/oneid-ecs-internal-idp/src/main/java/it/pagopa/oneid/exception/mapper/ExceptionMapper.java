9eb66654f82198fecb475d57e4c78b4abf549597
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2025-07-18 16:24:18 +0200

    feat(OI-374 internal-idp): exception handler mapped with error page and custom message

--- src/oneid/oneid-ecs-internal-idp/src/main/java/it/pagopa/oneid/exception/mapper/ExceptionMapper.java
+++ src/oneid/oneid-ecs-internal-idp/src/main/java/it/pagopa/oneid/exception/mapper/ExceptionMapper.java
@@ -1,68 +1,67 @@
 package it.pagopa.oneid.exception.mapper;
 
-import static jakarta.ws.rs.core.Response.Status.BAD_REQUEST;
-import static jakarta.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 import io.quarkus.logging.Log;
+import io.quarkus.qute.Template;
 import it.pagopa.oneid.exception.ClientNotFoundException;
 import it.pagopa.oneid.exception.IDPSessionException;
 import it.pagopa.oneid.exception.MalformedAuthnRequestException;
 import it.pagopa.oneid.exception.SAMLValidationException;
-import it.pagopa.oneid.model.ErrorResponse;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.jboss.resteasy.reactive.RestResponse;
-import org.jboss.resteasy.reactive.RestResponse.ResponseBuilder;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
 public class ExceptionMapper {
 
+  @Inject
+  Template error;
+
   @ServerExceptionMapper
-  public RestResponse<ErrorResponse> mapException(Exception exception) {
-    Response.Status status = INTERNAL_SERVER_ERROR;
-    String message = "Error during execution.";
+  public Response mapException(Exception exception) {
     Log.error(
         "mapException: " + ExceptionUtils.getStackTrace(exception));
-    return RestResponse.status(status, buildErrorResponse(status, message));
+    return buildErrorResponsePage("Si è verificato un errore inaspettato.");
   }
 
   @ServerExceptionMapper
-  public RestResponse<Object> mapClientNotFoundException(
+  public Response mapClientNotFoundException(
       ClientNotFoundException clientNotFoundException) {
     Log.error(
         "mapClientNotFoundException: " + ExceptionUtils.getStackTrace(clientNotFoundException));
-    return ResponseBuilder.create(BAD_REQUEST).build();
+    return buildErrorResponsePage("Il client specificato non è stato trovato.");
   }
 
   @ServerExceptionMapper
-  public RestResponse<Object> mapMalformedAuthnRequestException(
+  public Response mapMalformedAuthnRequestException(
       MalformedAuthnRequestException malformedAuthnRequestException) {
     Log.error(
         "mapMalformedAuthnRequestException: " + ExceptionUtils.getStackTrace(
             malformedAuthnRequestException));
-    return ResponseBuilder.create(BAD_REQUEST).build();
+    return buildErrorResponsePage("La richiesta di autenticazione non è valida.");
   }
 
   @ServerExceptionMapper
-  public RestResponse<Object> mapSAMLValidationException(
+  public Response mapSAMLValidationException(
       SAMLValidationException samlValidationException) {
     Log.error(
         "mapSAMLValidationException: " + ExceptionUtils.getStackTrace(samlValidationException));
-    return ResponseBuilder.create(BAD_REQUEST).build();
+    return buildErrorResponsePage("Errore nella validazione SAML.");
   }
 
   @ServerExceptionMapper
-  public RestResponse<Object> mapIDPSessionException(
+  public Response mapIDPSessionException(
       IDPSessionException idpSessionException) {
     Log.error(
         "mapIDPSessionException: " + ExceptionUtils.getStackTrace(idpSessionException));
-    return ResponseBuilder.create(BAD_REQUEST).build();
+    return buildErrorResponsePage("La sessione non è valida o è scaduta.");
   }
 
-  private ErrorResponse buildErrorResponse(Response.Status status, String message) {
-    return it.pagopa.oneid.model.ErrorResponse.builder()
-        .title(status.getReasonPhrase())
-        .status(status.getStatusCode())
-        .detail(message)
+  private Response buildErrorResponsePage(String errorMessage) {
+    return Response.status(Status.OK)
+        .entity(error.data("errorMessage", errorMessage))
+        .type(MediaType.TEXT_HTML)
         .build();
   }
 
