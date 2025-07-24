d774d38236a926385f82247e824289dca610710d
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2025-07-24 14:59:23 +0200

    refactor: remove useless utils functions and constants

--- src/oneid/oneid-control-panel/src/utils/constants.ts
+++ src/oneid/oneid-control-panel/src/utils/constants.ts
@@ -1,13 +1,1 @@
-import { ENV } from './env';
-
-export const BASE_ROUTE = ENV.PUBLIC_URL;
-
-export const ROUTE_LOGIN = '/login'; // should be /login
-export const ROUTE_LOGIN_SUCCESS = '/login/success';
-export const ROUTE_LOGIN_ERROR = '/login/error';
-export const ROUTE_LOGOUT = ENV.URL_FE.LOGOUT;
-
-export const IDP_PLACEHOLDER_IMG =
-  '/assets/idps/aHR0cHM6Ly92YWxpZGF0b3IuZGV2Lm9uZWlkLnBhZ29wYS5pdC9kZW1v.png';
-
 export const cognitoCustomAttribute = 'custom:client_id';
