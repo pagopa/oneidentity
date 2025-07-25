5d039143f58246693a025c6e2515e980edfe2b3b
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2025-07-25 17:00:06 +0200

    feat(admin panel): add fe pagination for user management table

--- src/oneid/oneid-control-panel/src/components/UserTable.tsx
+++ src/oneid/oneid-control-panel/src/components/UserTable.tsx
@@ -11,6 +11,7 @@ import {
   Typography,
   Paper,
   TextField,
+  TablePagination,
 } from '@mui/material';
 import {
   KeyboardArrowDown,
@@ -31,6 +32,19 @@ type Props = {
 const UserTable = ({ users, onDelete, onEdit }: Props) => {
   const [openRows, setOpenRows] = useState<Record<string, boolean>>({});
   const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(3);
+
+  const handleChangePage = (_event: unknown, newPage: number) => {
+    setCurrentPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(Number(event.target.value));
+    setCurrentPage(0);
+  };
 
   const filteredUsers = users.filter((user) =>
     user.username?.toLowerCase().includes(searchTerm.toLowerCase())
@@ -41,94 +55,119 @@ const UserTable = ({ users, onDelete, onEdit }: Props) => {
   };
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell colSpan={4}>
-              <Box display="flex" justifyContent="flex-end">
-                <TextField
-                  label="Cerca username"
-                  variant="outlined"
-                  size="small"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                />
-              </Box>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell />
-            <TableCell>Username</TableCell>
-            <TableCell>Password</TableCell>
-            <TableCell align="right">Actions</TableCell>
-          </TableRow>
-        </TableHead>
+    <Paper>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell colSpan={4}>
+                <Box display="flex" justifyContent="flex-end">
+                  <TextField
+                    label="Cerca username"
+                    variant="outlined"
+                    size="small"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                  />
+                </Box>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell />
+              <TableCell>Username</TableCell>
+              <TableCell>Password</TableCell>
+            </TableRow>
+          </TableHead>
 
-        <TableBody>
-          {filteredUsers.map((user) => {
-            const key = user.username ?? 'fallback-key';
-            return (
-              <Fragment key={key}>
-                <TableRow>
-                  <TableCell>
-                    <IconButton size="small" onClick={() => toggleRow(key)}>
-                      {openRows[key] ? (
-                        <KeyboardArrowUp />
-                      ) : (
-                        <KeyboardArrowDown />
-                      )}
-                    </IconButton>
-                  </TableCell>
-                  <TableCell>{user.username}</TableCell>
-                  <TableCell>{user.password}</TableCell>
-                  <TableCell align="right">
-                    <IconButton onClick={() => onEdit(user)} color="inherit">
-                      <Edit />
-                    </IconButton>
-                    <IconButton
-                      onClick={() => onDelete(key)}
-                      sx={{ color: 'error.main' }}
-                    >
-                      <Delete />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
+          <TableBody>
+            {filteredUsers
+              .slice(
+                currentPage * rowsPerPage,
+                currentPage * rowsPerPage + rowsPerPage
+              )
+              .map((user) => {
+                const key = user.username ?? 'fallback-key';
+                return (
+                  <Fragment key={key}>
+                    <TableRow>
+                      <TableCell>
+                        <IconButton size="small" onClick={() => toggleRow(key)}>
+                          {openRows[key] ? (
+                            <KeyboardArrowUp />
+                          ) : (
+                            <KeyboardArrowDown />
+                          )}
+                        </IconButton>
+                      </TableCell>
+                      <TableCell>{user.username}</TableCell>
+                      <TableCell>{user.password}</TableCell>
+                      <TableCell align="right">
+                        <IconButton
+                          onClick={() => onEdit(user)}
+                          color="inherit"
+                        >
+                          <Edit />
+                        </IconButton>
+                        <IconButton
+                          onClick={() => onDelete(key)}
+                          sx={{ color: 'error.main' }}
+                        >
+                          <Delete />
+                        </IconButton>
+                      </TableCell>
+                    </TableRow>
 
-                <TableRow>
-                  <TableCell
-                    colSpan={4}
-                    style={{ paddingBottom: 0, paddingTop: 0 }}
-                  >
-                    <Collapse in={openRows[key]} timeout="auto" unmountOnExit>
-                      <Box margin={2}>
-                        <Typography variant="subtitle1">
-                          SAML Attributes
-                        </Typography>
-                        {user.samlAttributes &&
-                        !isEmpty(user.samlAttributes) ? (
-                          <ul>
-                            {map(user.samlAttributes, (attrValue, attrKey) => (
-                              <li key={attrKey}>
-                                <strong>{attrKey}:</strong> {attrValue}
-                              </li>
-                            ))}
-                          </ul>
-                        ) : (
-                          <Typography variant="body2" color="textSecondary">
-                            No attributes
-                          </Typography>
-                        )}
-                      </Box>
-                    </Collapse>
-                  </TableCell>
-                </TableRow>
-              </Fragment>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                    <TableRow>
+                      <TableCell
+                        colSpan={4}
+                        style={{ paddingBottom: 0, paddingTop: 0 }}
+                      >
+                        <Collapse
+                          in={openRows[key]}
+                          timeout="auto"
+                          unmountOnExit
+                        >
+                          <Box margin={2}>
+                            <Typography variant="subtitle1">
+                              SAML Attributes
+                            </Typography>
+                            {user.samlAttributes &&
+                            !isEmpty(user.samlAttributes) ? (
+                              <ul>
+                                {map(
+                                  user.samlAttributes,
+                                  (attrValue, attrKey) => (
+                                    <li key={attrKey}>
+                                      <strong>{attrKey}:</strong> {attrValue}
+                                    </li>
+                                  )
+                                )}
+                              </ul>
+                            ) : (
+                              <Typography variant="body2" color="textSecondary">
+                                No attributes
+                              </Typography>
+                            )}
+                          </Box>
+                        </Collapse>
+                      </TableCell>
+                    </TableRow>
+                  </Fragment>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 50, 100]}
+        component="div"
+        count={filteredUsers.length}
+        rowsPerPage={rowsPerPage}
+        page={currentPage}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 };
 
