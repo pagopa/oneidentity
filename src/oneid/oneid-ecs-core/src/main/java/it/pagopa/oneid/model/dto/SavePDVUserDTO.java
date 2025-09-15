99cfaede464e0726bd3e06986e244af50201cc22
BenitoVisone <visonebenito@gmail.com>
2025-09-15 17:49:58 +0200

    fix: check if attributeValue is blank before using it into builder

--- src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/model/dto/SavePDVUserDTO.java
+++ src/oneid/oneid-ecs-core/src/main/java/it/pagopa/oneid/model/dto/SavePDVUserDTO.java
@@ -8,6 +8,7 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 @Data
 @Builder
@@ -96,119 +97,121 @@ public class SavePDVUserDTO {
     SavePDVUserDTO.SavePDVUserDTOBuilder builder = SavePDVUserDTO.builder();
 
     for (AttributeDTO attr : attributeDTOList) {
-      switch (attr.getAttributeName()) {
-        case "fiscalNumber":
-          builder.fiscalCode(attr.getAttributeValue().replace(FISCAL_CODE_PREFIX, ""));
-          break;
-        case "name":
-          builder.name(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "familyName":
-          builder.familyName(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "email":
-          builder.email(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "birthDate":
-          builder.birthDate(new CertifiedField<>(LocalDate.parse(attr.getAttributeValue())));
-          break;
-        case "spidCode":
-          builder.spidCode(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "placeOfBirth":
-          builder.placeOfBirth(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "countyOfBirth":
-          builder.countyOfBirth(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "gender":
-          builder.gender(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "companyName":
-          builder.companyName(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "registeredOffice":
-          builder.registeredOffice(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "ivaCode":
-          builder.ivaCode(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "idCard":
-          builder.idCard(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "mobilePhone":
-          builder.mobilePhone(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "address":
-          builder.address(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "expirationDate":
-          builder.expirationDate(new CertifiedField<>(LocalDate.parse(attr.getAttributeValue())));
-          break;
-        case "digitalAddress":
-          builder.digitalAddress(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "domicileAddress":
-          builder.domicileAddress(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "domicilePlace":
-          builder.domicilePlace(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "domicilePostalCode":
-          builder.domicilePostalCode(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "domicileProvince":
-          builder.domicileProvince(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "domicileCountry":
-          builder.domicileCountry(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "qualification":
-          builder.qualification(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "commonName":
-          builder.commonName(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "surname":
-          builder.surname(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "givenName":
-          builder.givenName(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "preferredUsername":
-          builder.preferredUsername(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "title":
-          builder.title(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "userCertificate":
-          builder.userCertificate(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "employeeNumber":
-          builder.employeeNumber(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "orgUnitName":
-          builder.orgUnitName(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "preferredLanguage":
-          builder.preferredLanguage(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "country":
-          builder.country(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "stateOrProvince":
-          builder.stateOrProvince(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "city":
-          builder.city(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "postalCode":
-          builder.postalCode(new CertifiedField<>(attr.getAttributeValue()));
-          break;
-        case "street":
-          builder.street(new CertifiedField<>(attr.getAttributeValue()));
-          break;
+      if (!StringUtils.isBlank(attr.getAttributeValue())) {
+        switch (attr.getAttributeName()) {
+          case "fiscalNumber":
+            builder.fiscalCode(attr.getAttributeValue().replace(FISCAL_CODE_PREFIX, ""));
+            break;
+          case "name":
+            builder.name(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "familyName":
+            builder.familyName(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "email":
+            builder.email(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "birthDate":
+            builder.birthDate(new CertifiedField<>(LocalDate.parse(attr.getAttributeValue())));
+            break;
+          case "spidCode":
+            builder.spidCode(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "placeOfBirth":
+            builder.placeOfBirth(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "countyOfBirth":
+            builder.countyOfBirth(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "gender":
+            builder.gender(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "companyName":
+            builder.companyName(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "registeredOffice":
+            builder.registeredOffice(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "ivaCode":
+            builder.ivaCode(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "idCard":
+            builder.idCard(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "mobilePhone":
+            builder.mobilePhone(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "address":
+            builder.address(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "expirationDate":
+            builder.expirationDate(new CertifiedField<>(LocalDate.parse(attr.getAttributeValue())));
+            break;
+          case "digitalAddress":
+            builder.digitalAddress(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "domicileAddress":
+            builder.domicileAddress(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "domicilePlace":
+            builder.domicilePlace(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "domicilePostalCode":
+            builder.domicilePostalCode(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "domicileProvince":
+            builder.domicileProvince(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "domicileCountry":
+            builder.domicileCountry(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "qualification":
+            builder.qualification(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "commonName":
+            builder.commonName(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "surname":
+            builder.surname(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "givenName":
+            builder.givenName(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "preferredUsername":
+            builder.preferredUsername(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "title":
+            builder.title(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "userCertificate":
+            builder.userCertificate(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "employeeNumber":
+            builder.employeeNumber(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "orgUnitName":
+            builder.orgUnitName(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "preferredLanguage":
+            builder.preferredLanguage(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "country":
+            builder.country(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "stateOrProvince":
+            builder.stateOrProvince(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "city":
+            builder.city(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "postalCode":
+            builder.postalCode(new CertifiedField<>(attr.getAttributeValue()));
+            break;
+          case "street":
+            builder.street(new CertifiedField<>(attr.getAttributeValue()));
+            break;
 
+        }
       }
     }
 
