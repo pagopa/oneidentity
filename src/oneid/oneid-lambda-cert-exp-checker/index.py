0013314a16333c4883f643c5c3233a7568da0872
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2025-09-25 16:49:24 +0200

    feat: Lambda to check cert expiration (#869)

---
+++ src/oneid/oneid-lambda-cert-exp-checker/index.py
@@ -0,0 +1,32 @@
+import boto3
+import os
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+import datetime
+
+param_name = os.environ['PARAM_NAME']
+sns_topic  = os.environ['SNS_TOPIC']
+
+def lambda_handler(event, context):
+    ssm = boto3.client('ssm')
+    sns = boto3.client('sns')
+
+    # Get certificate from Parameter Store
+    param = ssm.get_parameter(Name= param_name, WithDecryption=True)
+    cert_pem = param['Parameter']['Value'].encode()
+
+    # Parse certificate
+    cert = x509.load_pem_x509_certificate(cert_pem, default_backend())
+    expiry_date = cert.not_valid_after
+    days_left = (expiry_date - datetime.datetime.utcnow()).days
+
+    if days_left <= 15:
+        message = f"The OneIdentity's certificate '{param['Parameter']['Name']}' expires in {days_left} days on {expiry_date}."
+        sns.publish(
+            TopicArn=sns_topic,
+            Subject='⚠️ OneIdentity signing Certificate Expiry Warning',
+            Message=message
+        )
+        print("Notification sent.")
+    else:
+        print(f"Certificate is valid for {days_left} more days.")
