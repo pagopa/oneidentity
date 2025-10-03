fe4164cb7fe9d04feac97ca64235119fb0e1031b
BenSca <benedetto.scala@pagopa.it>
2025-10-03 14:22:28 +0200

    feat: add get plans and validate api in pdv client

--- src/oneid/oneid-common/connector/src/main/java/it/pagopa/oneid/common/connector/PDVApiClient.java
+++ src/oneid/oneid-common/connector/src/main/java/it/pagopa/oneid/common/connector/PDVApiClient.java
@@ -1,9 +1,14 @@
 package it.pagopa.oneid.common.connector;
 
+import it.pagopa.oneid.common.model.dto.PDVApiKeysDTO;
 import it.pagopa.oneid.common.model.dto.PDVUserUpsertResponseDTO;
+import it.pagopa.oneid.common.model.dto.PDVValidateApiKeyDTO;
+import it.pagopa.oneid.common.model.dto.PDVValidationResponseDTO;
 import it.pagopa.oneid.common.model.dto.SavePDVUserDTO;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
@@ -19,4 +24,16 @@ public interface PDVApiClient {
       @HeaderParam("x-api-key") String apiKey
   );
 
+  @GET
+  @Path("/plan-details/api-keys")
+  PDVApiKeysDTO getPDVPlans(
+      @HeaderParam("x-api-key") String apiKey
+  );
+
+  @POST
+  @Path("/plan-details/validate-api-key")
+  PDVValidationResponseDTO validatePDVApiKey(
+      @RequestBody PDVValidateApiKeyDTO validateApiKeyDTO,
+      @HeaderParam("x-api-key") String apiKey
+  );
 }
