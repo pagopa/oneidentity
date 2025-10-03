c7453e85644207b4f4749814a9420c789b6ad705
BenSca <benedetto.scala@pagopa.it>
2025-10-03 14:22:28 +0200

    feat: add get plans and validate api in pdv client

---
+++ src/oneid/oneid-common/model/src/main/java/it/pagopa/oneid/common/model/dto/PDVValidateApiKeyDTO.java
@@ -0,0 +1,24 @@
+package it.pagopa.oneid.common.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+@JsonInclude(Include.NON_NULL)
+public class PDVValidateApiKeyDTO {
+
+  @NotNull
+  @JsonProperty("api_key_id")
+  private String apiKeyId;
+
+  @NotNull
+  @JsonProperty("api_key_value")
+  private String apiKeyValue;
+}
