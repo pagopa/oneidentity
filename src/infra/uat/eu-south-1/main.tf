e1e584c6200f4ee18ebfdc3843bf8c1df9dc5e65
BenSca <120383035+BenSca@users.noreply.github.com>
2025-10-28 15:21:57 +0100

    feat: add infra pdv plan url variables

--- src/infra/uat/eu-south-1/main.tf
+++ src/infra/uat/eu-south-1/main.tf
@@ -366,7 +366,8 @@ module "backend" {
       "LOG_LEVEL"                          = var.app_log_level
       "SNS_TOPIC_ARN"                      = module.sns.sns_topic_arn
       "SNS_TOPIC_NOTIFICATION_ENVIRONMENT" = var.env_short
-      "QUARKUS_HTTP_CORS_ORIGINS"          = "https://admin.uat.oneid.pagopa.it"
+      "QUARKUS_HTTP_CORS_ORIGINS"          = "https://admin.uat.oneid.pagopa.it",
+      "PDV_PLAN_URL"                       = var.pdv_plan_url
     }
   }
 
