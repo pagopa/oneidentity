c39dd6a9c4bc00d9473d6fbd666bf34b5b05a7fc
BenSca <120383035+BenSca@users.noreply.github.com>
2025-10-28 15:18:17 +0100

    refactor: remove unused imports

--- src/oneid/oneid-common/connector/src/main/java/it/pagopa/oneid/common/connector/PDVApiPlanClient.java
+++ src/oneid/oneid-common/connector/src/main/java/it/pagopa/oneid/common/connector/PDVApiPlanClient.java
@@ -1,13 +1,10 @@
 package it.pagopa.oneid.common.connector;
 
 import it.pagopa.oneid.common.model.dto.PDVApiKeysDTO;
-import it.pagopa.oneid.common.model.dto.PDVUserUpsertResponseDTO;
 import it.pagopa.oneid.common.model.dto.PDVValidateApiKeyDTO;
 import it.pagopa.oneid.common.model.dto.PDVValidationResponseDTO;
-import it.pagopa.oneid.common.model.dto.SavePDVUserDTO;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.HeaderParam;
-import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
