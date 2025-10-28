1a270dc58290416076fe43b6c2af90752554277a
BenSca <120383035+BenSca@users.noreply.github.com>
2025-10-28 18:52:08 +0100

    refactor: pdv exception

--- src/oneid/oneid-common/connector/src/main/java/it/pagopa/oneid/common/connector/exception/PDVException.java
+++ src/oneid/oneid-common/connector/src/main/java/it/pagopa/oneid/common/connector/exception/PDVException.java
@@ -1,39 +1,24 @@
 package it.pagopa.oneid.common.connector.exception;
 
 import jakarta.ws.rs.WebApplicationException;
+import java.util.Optional;
 
 public class PDVException extends WebApplicationException {
 
   private final Integer status;
-  private final String payload;
+  private final Optional<String> payload;
 
-  public PDVException(String message, Integer status, String payload, Throwable cause) {
+  public PDVException(String message, Integer status, Optional<String> payload, Throwable cause) {
     super(message, cause);
     this.status = status;
     this.payload = payload;
-  }
-
-  public PDVException(String message, WebApplicationException cause) {
-    super(message, cause,
-        cause.getResponse() != null ? cause.getResponse() : null);
-    Integer s = null;
-    String body = null;
-    if (cause.getResponse() != null) {
-      s = cause.getResponse().getStatus();
-      try {
-        body = cause.getResponse().readEntity(String.class);
-      } catch (Exception ignored) {
-      }
-    }
-    this.status = s;
-    this.payload = body;
   }
 
   public Integer getStatus() {
     return status;
   }
 
-  public String getPayload() {
+  public Optional<String> getPayload() {
     return payload;
   }
 }
