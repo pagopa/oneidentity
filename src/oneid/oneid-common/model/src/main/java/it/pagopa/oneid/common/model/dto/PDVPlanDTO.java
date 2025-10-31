76549666b538c57b37b01a962fe5344a0bff558d
BenSca <120383035+BenSca@users.noreply.github.com>
2025-10-31 16:26:41 +0100

    refactor: remove useless @AllArgsConstructor

--- src/oneid/oneid-common/model/src/main/java/it/pagopa/oneid/common/model/dto/PDVPlanDTO.java
+++ src/oneid/oneid-common/model/src/main/java/it/pagopa/oneid/common/model/dto/PDVPlanDTO.java
@@ -1,14 +1,12 @@
 package it.pagopa.oneid.common.model.dto;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @RegisterForReflection
 @SuperBuilder
 public class PDVPlanDTO {
