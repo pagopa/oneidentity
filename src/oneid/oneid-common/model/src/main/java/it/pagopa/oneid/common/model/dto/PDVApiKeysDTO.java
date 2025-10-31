484b643eb5c4b15be6f5b7150567ec2dac470e17
BenSca <120383035+BenSca@users.noreply.github.com>
2025-10-31 16:26:41 +0100

    refactor: remove useless @AllArgsConstructor

--- src/oneid/oneid-common/model/src/main/java/it/pagopa/oneid/common/model/dto/PDVApiKeysDTO.java
+++ src/oneid/oneid-common/model/src/main/java/it/pagopa/oneid/common/model/dto/PDVApiKeysDTO.java
@@ -3,14 +3,12 @@ package it.pagopa.oneid.common.model.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @RegisterForReflection
 @SuperBuilder
 public class PDVApiKeysDTO {
