d1e1dbcec4070788016de22e372059407592e711
BenSca <120383035+BenSca@users.noreply.github.com>
2025-11-11 12:40:24 +0100

    test: register ut

--- src/oneid/oneid-control-panel/src/api/register.test.ts
+++ src/oneid/oneid-control-panel/src/api/register.test.ts
@@ -1,7 +1,19 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest';
-import { getClientData, createOrUpdateClient } from './register';
+import {
+  getClientData,
+  createOrUpdateClient,
+  getPlanList,
+  validateApiKeyPlan,
+} from './register';
 import { ENV } from '../utils/env';
-import { Client, SamlAttribute, SpidLevel } from '../types/api';
+import {
+  Client,
+  SamlAttribute,
+  SpidLevel,
+  validApiKeySchema,
+  ValidateApiKeySchema,
+  ValidatePlanSchema,
+} from '../types/api';
 
 vi.mock('../utils/env', () => ({
   ENV: { URL_API: { REGISTER: 'https://api.example.com/register' } },
@@ -53,7 +65,7 @@ describe('getClientData', () => {
     vi.restoreAllMocks();
   });
 
-  it('fetches client data successfully', async () => {
+  it('fetches plan data successfully', async () => {
     axiosMock.get.mockResolvedValueOnce({
       data: mockClientData,
     });
@@ -151,6 +163,43 @@ describe('createOrUpdateClient', () => {
     );
   });
 
+  it('updates an existing client successfully pairwise enabled', async () => {
+    const clientId = 'existing-client-id';
+    axiosMock.put.mockResolvedValueOnce({
+      data: {
+        ...mockClientData,
+        pairwise: true,
+      },
+    });
+
+    const mockValidatePlanSchema: ValidatePlanSchema = {
+      apiKeyId: 'plan1',
+      apiKeyValue: 'keyValue',
+    };
+
+    const result = await createOrUpdateClient(
+      mockClientData,
+      token,
+      clientId,
+      mockValidatePlanSchema
+    );
+    expect(result).toEqual({
+      ...mockClientData,
+      pairwise: true,
+    });
+    expect(axiosMock.put).toHaveBeenCalledWith(
+      `${ENV.URL_API.REGISTER}/client_id/${clientId}`,
+      mockClientData,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'PDV-X-Api-Key': mockValidatePlanSchema.apiKeyValue,
+          'PDV-Plan-Name': mockValidatePlanSchema.apiKeyId,
+        },
+      }
+    );
+  });
+
   it('throws validation errors for invalid client data', async () => {
     const invalidData = { ...mockClientData, client_name: '' }; // Invalid client_name
     await expect(createOrUpdateClient(invalidData, token)).rejects.toThrow();
@@ -172,6 +221,131 @@ describe('createOrUpdateClient', () => {
     axiosMock.post.mockRejectedValueOnce(mockError);
 
     await expect(createOrUpdateClient(mockClientData, token)).rejects.toThrow(
+      'Unexpected error'
+    );
+  });
+});
+describe('getPlanData', () => {
+  const clientID = '0000000000000000000000000000000000000000000';
+
+  const mockPlanData = {
+    api_keys: [
+      {
+        id: 'id1',
+        name: 'plan1',
+      },
+      {
+        id: 'id2',
+        name: 'plan2',
+      },
+    ],
+  };
+
+  beforeEach(() => {
+    vi.stubGlobal('window', {
+      location: { search: `?client_id=${clientID}` },
+    });
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('fetches plan data successfully', async () => {
+    axiosMock.get.mockResolvedValueOnce({
+      data: mockPlanData,
+    });
+
+    const result = await getPlanList('token');
+    expect(result).toEqual(mockPlanData);
+  });
+
+  it('throws a custom error if the fetch fails', async () => {
+    axiosMock.get.mockRejectedValue({
+      isAxiosError: true,
+      response: { data: { status: 401, message: 'Unauthorized' } },
+    });
+
+    await expect(getPlanList('token')).rejects.toThrow('Unauthorized');
+  });
+
+  it('throws an error if the fetch fails', async () => {
+    axiosMock.get.mockRejectedValue({
+      isAxiosError: true,
+      response: { data: { status: 401 } },
+    });
+
+    await expect(getPlanList('token')).rejects.toThrow(
+      'Failed to fetch plan data'
+    );
+  });
+  it('throws a generic error if the fetch fails', async () => {
+    const mockError = {
+      isAxiosError: false,
+      response: { data: { status: 401 } },
+    };
+    axiosMock.get.mockRejectedValue(mockError);
+
+    await expect(getPlanList('token')).rejects.toThrow(
+      `An unknown error occurred ${JSON.stringify(mockError)}`
+    );
+  });
+});
+
+describe('validateApiKeyPlan', () => {
+  const mockValidateApiKeyData: ValidateApiKeySchema = {
+    valid: true,
+  };
+
+  const mockValidatePlan: ValidatePlanSchema = {
+    apiKeyId: 'plan1',
+    apiKeyValue: 'keyValue',
+  };
+
+  const token = 'test-token';
+
+  it('validates key successfully', async () => {
+    axiosMock.post.mockResolvedValueOnce({
+      data: {
+        ...mockValidateApiKeyData,
+      },
+    });
+
+    const result = await validateApiKeyPlan(mockValidatePlan, token);
+    expect(result).toEqual({
+      ...mockValidateApiKeyData,
+    });
+    expect(axiosMock.post).toHaveBeenCalledWith(
+      ENV.URL_API.REGISTER + ENV.URL_API.VALIDATE_API_PLAN,
+      {
+        api_key_id: mockValidatePlan.apiKeyId,
+        api_key_value: mockValidatePlan.apiKeyValue,
+      },
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+  });
+
+  it('throws validation errors for invalid API key data', async () => {
+    const invalidData = { ...mockValidatePlan, apiKeyValue: '' };
+    await expect(validateApiKeyPlan(invalidData, token)).rejects.toThrow();
+  });
+
+  it('throws an error if the API call fails', async () => {
+    axiosMock.post.mockRejectedValueOnce({
+      isAxiosError: true,
+      response: { data: { message: 'Failed to validate API key' } },
+    });
+
+    await expect(validateApiKeyPlan(mockValidatePlan, token)).rejects.toThrow(
+      'Failed to validate API key'
+    );
+  });
+
+  it('throws a generic error if the API call fails unexpectedly', async () => {
+    const mockError = { isAxiosError: false, message: 'Unexpected error' };
+    axiosMock.post.mockRejectedValueOnce(mockError);
+
+    await expect(validateApiKeyPlan(mockValidatePlan, token)).rejects.toThrow(
       'Unexpected error'
     );
   });
