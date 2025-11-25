3c2f50213181cf0bf6bd94c59f4ed97d6d21f8bf
BenSca <120383035+BenSca@users.noreply.github.com>
2025-11-25 12:40:47 +0100

    refactor. use ConfirmDialog in dashaboard and manage text including pairwise info

--- src/oneid/oneid-control-panel/src/pages/Dashboard/Dashboard.tsx
+++ src/oneid/oneid-control-panel/src/pages/Dashboard/Dashboard.tsx
@@ -16,11 +16,6 @@ import {
   FormHelperText,
   FormControlLabel,
   Switch,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
   Link,
   Divider,
 } from '@mui/material';
@@ -49,6 +44,7 @@ import { PageContainer } from '../../components/PageContainer';
 import { ContentBox } from '../../components/ContentBox';
 import FieldWithInfo from '../../components/FieldWithInfo';
 import { tooltipLinkSx } from '../../utils/styles';
+import ConfirmDialog from '../../components/ConfirmDialog';
 
 export const Dashboard = () => {
   const [formData, setFormData] =
@@ -64,8 +60,15 @@ export const Dashboard = () => {
   const [isConfirmOpen, setIsConfirmOpen] = useState(false);
   const openConfirm = () => setIsConfirmOpen(true);
   const closeConfirm = () => setIsConfirmOpen(false);
+  const [contentDialog, setContentDialog] = useState<string>('');
   const { isModalOpen, openModal, closeModal } = useModalManager();
-
+  const enablingPairWiseDialogContent =
+    'By enabling PairWise One Identity, it will use PDV to obtain the subject identifier.';
+  const disablingPairWiseDialogContent =
+    'Disabling PairWise pseudonymous will stop sharing communication from PDV Building Block and OI.';
+  const metadataDialogContent =
+    'Updating SPID Level and SAML Attributes fields may require re-sharing them with the SPID and CIE authorities (AgID and IPZS).';
+  const confirmText = 'Please confirm you want to proceed';
   const { setClientId, clientId } = useClientId();
 
   const {
@@ -114,7 +117,7 @@ export const Dashboard = () => {
 
   const validationIsValid = validationResult?.valid;
 
-  useEffect(() => {
+useEffect(() => {
     if (updateError) {
       console.error('Error updating client:', updateError);
       setErrorUi(updateError as unknown as ClientErrors);
@@ -230,22 +233,45 @@ export const Dashboard = () => {
     }
   };
 
+  const createContentDialog = () => {
+    const isPairwiseChanged = !isEqual(
+      formData?.pairwise,
+      fetchedClientData?.pairwise
+    );
+
+    const isMetadataChanged =
+      !isEqual(
+        formData?.samlRequestedAttributes,
+        fetchedClientData?.samlRequestedAttributes
+      ) ||
+      !isEqual(formData?.defaultAcrValues, fetchedClientData?.defaultAcrValues);
+
+    const pairWiseText =
+      isPairwiseChanged &&
+      (formData?.pairwise
+        ? enablingPairWiseDialogContent
+        : disablingPairWiseDialogContent);
+
+    const changeType = `${isPairwiseChanged ? 'pairwise' : ''}${isPairwiseChanged && isMetadataChanged ? '+' : ''}${isMetadataChanged ? 'metadata' : ''}`;
+    switch (changeType) {
+      case 'pairwise+metadata':
+        return `${metadataDialogContent}\n\n${pairWiseText} ${confirmText}`;
+      case 'pairwise':
+        return `${pairWiseText} ${confirmText}`;
+      case 'metadata':
+        return `${metadataDialogContent} ${confirmText}`;
+      default:
+        return '';
+    }
+  };
+
   const handleSubmit = async (e: React.FormEvent) => {
     // open modal
     e.preventDefault();
-
+    const content = createContentDialog();
+    setContentDialog(content || '');
     // open modal only if is update stauts && spid level or saml attributes are modified
-    if (
-      clientId &&
-      (!isEqual(
-        formData?.defaultAcrValues,
-        fetchedClientData?.defaultAcrValues
-      ) ||
-        !isEqual(
-          formData?.samlRequestedAttributes,
-          fetchedClientData?.samlRequestedAttributes
-        ))
-    ) {
+    if (clientId && content !== '') {
       openConfirm();
     } else {
       doSubmit();
@@ -636,33 +662,22 @@ export const Dashboard = () => {
           </Button>
         </Box>
       </Box>
-      <Dialog
+      <ConfirmDialog
         open={isConfirmOpen}
-        onClose={closeConfirm}
-        aria-labelledby="confirm-submit-title"
-      >
-        <DialogTitle id="confirm-submit-title">Confirm changes</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Updating SPID Level and SAML Attributes fields may require
-            re-sharing them with the SPID and CIE authorities (AgID and IPZS).
-            Please confirm you want to proceed
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={closeConfirm} disabled={isUpdating}>
-            Cancel
-          </Button>
-          <Button
-            onClick={handleConfirmSubmit}
-            variant="contained"
-            autoFocus
-            disabled={isUpdating}
-          >
-            {isUpdating ? 'Saving...' : 'Confirm'}
-          </Button>
-        </DialogActions>
-      </Dialog>
+        title="Confirm changes"
+        content={contentDialog}
+        onCancel={closeConfirm}
+        onConfirm={handleConfirmSubmit}
+        confirmText={isUpdating ? 'Saving...' : 'Confirm'}
+        cancelText="Cancel"
+        confirmButtonProps={{
+          color: 'primary',
+          variant: 'contained',
+        }}
+        cancelButtonProps={{
+          color: 'primary',
+        }}
+      />
       <Notify
         open={notify.open}
         message={notify.message}
