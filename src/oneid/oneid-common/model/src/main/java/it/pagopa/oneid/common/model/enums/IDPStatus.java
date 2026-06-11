188e871c38839ce48c8d5b3261908d72f9f49b9a
giuseppe-gangemi <giuseppe.gangemi@pagopa.it>
2026-06-11 12:40:00 +0200

    fix: convert danger enum to KO to avoid conflicts error on the dynamodb table

--- src/oneid/oneid-common/model/src/main/java/it/pagopa/oneid/common/model/enums/IDPStatus.java
+++ src/oneid/oneid-common/model/src/main/java/it/pagopa/oneid/common/model/enums/IDPStatus.java
@@ -1,7 +1,8 @@
 package it.pagopa.oneid.common.model.enums;
 
+//TODO: create a default or a mechanism that avoid a error inside ecs-core in case of not mapped status
 public enum IDPStatus {
   OK,
   WARNING,
-  DANGER;
+  KO
 }
