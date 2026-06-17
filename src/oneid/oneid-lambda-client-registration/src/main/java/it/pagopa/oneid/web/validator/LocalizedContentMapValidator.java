23878fb80456c3fe9c47cb628f4032685e9eed44
BenSca <120383035+BenSca@users.noreply.github.com>
2026-06-17 15:16:34 +0200

    chore: use new validator in LocalizedContentMapValidator

--- src/oneid/oneid-lambda-client-registration/src/main/java/it/pagopa/oneid/web/validator/LocalizedContentMapValidator.java
+++ src/oneid/oneid-lambda-client-registration/src/main/java/it/pagopa/oneid/web/validator/LocalizedContentMapValidator.java
@@ -3,19 +3,19 @@ package it.pagopa.oneid.web.validator;
 import static it.pagopa.oneid.service.utils.ValidationUtils.isSafeDescription;
 import static it.pagopa.oneid.service.utils.ValidationUtils.isSafeTitle;
 import it.pagopa.oneid.common.model.Client.LocalizedContent;
-import it.pagopa.oneid.exception.InvalidUriException;
-import it.pagopa.oneid.service.utils.CustomURIUtils;
 import it.pagopa.oneid.web.validator.annotations.LocalizedContentMapCheck;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import java.util.Map;
 import java.util.Set;
-import org.apache.commons.lang3.StringUtils;
 
 public class LocalizedContentMapValidator implements
     ConstraintValidator<LocalizedContentMapCheck, Map<String, Map<String, LocalizedContent>>> {
 
   private static final Set<String> ALLOWED_LANGS = Set.of("it", "fr", "de", "sl", "en");
+  private final SafeUriValidator safeUriValidator = new SafeUriValidator();
+  private final SafeHttpsOrMailValidator safeHttpsOrMailValidator = new SafeHttpsOrMailValidator();
+  private final SafeEmailValidator safeEmailValidator = new SafeEmailValidator();
 
   @Override
   public void initialize(LocalizedContentMapCheck constraintAnnotation) {
@@ -55,22 +55,15 @@ public class LocalizedContentMapValidator implements
 
         boolean contentValid = isSafeTitle(content.title(), 10)
             && isSafeDescription(content.description());
-        
-        try {
-          if (content.cookieUri()!=null && StringUtils.isNotBlank(content.cookieUri())) {
-            CustomURIUtils.validateURI(content.cookieUri());
-          }
-          if (content.docUri()!=null && StringUtils.isNotBlank(content.docUri())) {
-            CustomURIUtils.validateHttpsOrMail(content.docUri());
-          }
-          if (content.supportAddress()!=null && StringUtils.isNotBlank(content.supportAddress())) {
-            CustomURIUtils.validateEmail(content.supportAddress());
-          }
-        } catch (InvalidUriException e) {
-          return false;
-        }
 
-        return langValid && contentValid;
+        boolean cookieUriValid = safeUriValidator.isValid(content.cookieUri(),
+            constraintValidatorContext);
+        boolean docUriValid = safeHttpsOrMailValidator.isValid(content.docUri(),
+            constraintValidatorContext);
+        boolean supportAddressValid = safeEmailValidator.isValid(content.supportAddress(),
+            constraintValidatorContext);
+
+        return langValid && contentValid && cookieUriValid && docUriValid && supportAddressValid;
       });
     });
   }
