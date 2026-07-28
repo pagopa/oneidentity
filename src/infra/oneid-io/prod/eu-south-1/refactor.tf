913fc405ab690d115c2f027fad0f89fd31e8e26d
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2026-07-28 11:43:30 +0200

    feat: update refactor.tf to standardize moved resource syntax

--- src/infra/oneid-io/prod/eu-south-1/refactor.tf
+++ src/infra/oneid-io/prod/eu-south-1/refactor.tf
@@ -5,49 +5,44 @@ import {
   id = "key.pem"
 }
 
-import {
-  to = module.frontend.module.rest_api_admin[0].aws_api_gateway_authorizer.main[0]
-  id = "sll5rnozck/o1bah5"
-}
+# ## Move ##
 
-## Move ##
+# moved {
+#   from = module.r53_zones.aws_route53_zone.this["io.oneid.pagopa.it"]
+#   to   = module.r53_zones.module.r53_zones.aws_route53_zone.this["io.oneid.pagopa.it"]
+# }
 
-moved {
-  from = module.r53_zones.aws_route53_zone.this["oneid.pagopa.it"]
-  to   = module.r53_zones.module.r53_zones.aws_route53_zone.this["oneid.pagopa.it"]
-}
+# moved {
+#   from = module.storage.module.s3_athena_output_bucket
+#   to   = module.storage.module.s3_athena_output_bucket[0]
+# }
 
-moved {
-  from = module.storage.module.s3_athena_output_bucket
-  to   = module.storage.module.s3_athena_output_bucket[0]
-}
+# moved {
+#   from = module.database.module.dynamodb_table_idpMetadata
+#   to   = module.database.module.dynamodb_table_idpMetadata[0]
+# }
 
-moved {
-  from = module.database.module.dynamodb_table_idpMetadata
-  to   = module.database.module.dynamodb_table_idpMetadata[0]
-}
+# moved {
+#   from = module.database.module.dynamodb_table_client_registrations
+#   to   = module.database.module.dynamodb_table_client_registrations[0]
+# }
 
-moved {
-  from = module.database.module.dynamodb_table_client_registrations
-  to   = module.database.module.dynamodb_table_client_registrations[0]
-}
+# moved {
+#   from = module.frontend.module.records.aws_route53_record.this[" A"]
+#   to   = module.frontend.module.records[0].aws_route53_record.this[" A"]
+# }
 
-moved {
-  from = module.frontend.module.records.aws_route53_record.this[" A"]
-  to   = module.frontend.module.records[0].aws_route53_record.this[" A"]
-}
+# moved {
+#   from = module.storage.module.s3_assets_bucket
+#   to   = module.storage.module.s3_assets_bucket[0]
+# }
 
-moved {
-  from = module.storage.module.s3_assets_bucket
-  to   = module.storage.module.s3_assets_bucket[0]
-}
+# moved {
+#   from = module.storage.aws_iam_policy.github_s3_policy
+#   to   = module.storage.aws_iam_policy.github_s3_policy[0]
+# }
 
-moved {
-  from = module.storage.aws_iam_policy.github_s3_policy
-  to   = module.storage.aws_iam_policy.github_s3_policy[0]
-}
-
-moved {
-  from = module.storage.module.s3_idp_metadata_bucket
-  to   = module.storage.module.s3_idp_metadata_bucket[0]
-}
+# moved {
+#   from = module.storage.module.s3_idp_metadata_bucket
+#   to   = module.storage.module.s3_idp_metadata_bucket[0]
+# }
