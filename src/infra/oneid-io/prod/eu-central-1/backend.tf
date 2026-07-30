b6c5ab840f81a1db81b9b713928a2c9df0c99701
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2026-07-30 15:58:12 +0200

    feat: Add Terraform configuration files for oneid-io in eu-central-1

---
+++ src/infra/oneid-io/prod/eu-central-1/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-1785162376"
+    key            = "prod-io/eu-central-1/main/tfstate"
+    region         = "eu-south-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
