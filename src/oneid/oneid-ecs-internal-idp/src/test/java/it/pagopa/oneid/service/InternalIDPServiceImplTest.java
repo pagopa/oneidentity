69f17f4aa4faaaaaed828f9dfcbe6fc4e0cda6c9
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2026-08-05 15:26:48 +0200

    test: refactor SSM mock certificate and key for clarity

--- src/oneid/oneid-ecs-internal-idp/src/test/java/it/pagopa/oneid/service/InternalIDPServiceImplTest.java
+++ src/oneid/oneid-ecs-internal-idp/src/test/java/it/pagopa/oneid/service/InternalIDPServiceImplTest.java
@@ -348,57 +348,56 @@ class InternalIDPServiceImplTest {
         .build();
   }
 
-  // region SSM mock — self-signed RSA cert+key (openssl req -x509 / pkcs8, test-only) // gitleaks:allow
-  private static final String TEST_CERT_PEM =
-      "-----BEGIN CERTIFICATE-----\n" // gitleaks:allow
-          + "MIIDBTCCAe2gAwIBAgIUa+M221BaG6NKPx8OSrKjqE4tvYAwDQYJKoZIhvcNAQEL\n"
-          + "BQAwEjEQMA4GA1UEAwwHVGVzdElEUDAeFw0yNjA4MDUxMjMzMzlaFw0zNjA4MDIx\n"
-          + "MjMzMzlaMBIxEDAOBgNVBAMMB1Rlc3RJRFAwggEiMA0GCSqGSIb3DQEBAQUAA4IB\n"
-          + "DwAwggEKAoIBAQCvyidJ8iaM93ymiTqH/GOZWzU7/bR6bYMK/czHiHZNLgmUARgE\n"
-          + "1rXWxsrQ5vrnVgHKkg49+AX84L+BAjh/e4djzOquRWn/arw8oo7TWmmqFG1emkLD\n"
-          + "5qNqENdIRp22QdAd61FaXEOfhiKs24LqzKNl6W1s3JXEB0hVjw8Bc5IkjDBz8zgN\n"
-          + "Cz2qViG3bZ7UAbSQowGwd8adMnt/D3GrKgt1FR+mZZHNCui0DEl4wsqX8CuYwy5m\n"
-          + "NmI+ui5Sq/VDeNhiMAvew+tmAcz4rGJWbsmF8NEufSS52iGBOLOfgcKDq4yZ/CTL\n"
-          + "XLhs9LIOOMejZcESMM69S3XE7mgkRumPJPshAgMBAAGjUzBRMB0GA1UdDgQWBBRt\n"
-          + "QxlUV3gH182o6L6ooYCsA4mOeDAfBgNVHSMEGDAWgBRtQxlUV3gH182o6L6ooYCs\n"
-          + "A4mOeDAPBgNVHRMBAf8EBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQATJv9Rbz7L\n"
-          + "E5iRxLaCttlVWDoPDKaKKLS2/W+knhnlO9dp6guNMg6ZdNj6JgYNktkqFRqFDwR+\n"
-          + "upWtBmY/IJboNavfbGiGOsg45H9scf9Iq0uH8zPCJKxJAfbQyqQ1zwTDWMeXkmEy\n"
-          + "mublHy0rlIrFRbjHpi05e6q2u2Efe2HEW0IVnmdXK44AJUu9umf2Zhy+iY/HBwm3\n"
-          + "jVmp1nU/Blm4ojXDR/oa0o+JRGuXFvJyKmODQQQoul5wME/p5TKw5JSX68t870o+\n"
-          + "8gG8sjBqWagKI9VsO5yBlEKGHjIlSmNjNEh5GfHwehMMAb0kO467MxuBP0iuNIhQ\n"
-          + "Bb+3Jm9l+t+3\n"
-          + "-----END CERTIFICATE-----";
+  // region SSM mock — self-signed RSA cert+key (openssl req -x509 / pkcs8,
+  // test-only) // gitleaks:allow
+  private static final String TEST_CERT_PEM = "-----BEGIN CERTIFICATE-----\n" // gitleaks:allow
+      + "MIIDBTCCAe2gAwIBAgIUa+M221BaG6NKPx8OSrKjqE4tvYAwDQYJKoZIhvcNAQEL\n"
+      + "BQAwEjEQMA4GA1UEAwwHVGVzdElEUDAeFw0yNjA4MDUxMjMzMzlaFw0zNjA4MDIx\n"
+      + "MjMzMzlaMBIxEDAOBgNVBAMMB1Rlc3RJRFAwggEiMA0GCSqGSIb3DQEBAQUAA4IB\n"
+      + "DwAwggEKAoIBAQCvyidJ8iaM93ymiTqH/GOZWzU7/bR6bYMK/czHiHZNLgmUARgE\n"
+      + "1rXWxsrQ5vrnVgHKkg49+AX84L+BAjh/e4djzOquRWn/arw8oo7TWmmqFG1emkLD\n"
+      + "5qNqENdIRp22QdAd61FaXEOfhiKs24LqzKNl6W1s3JXEB0hVjw8Bc5IkjDBz8zgN\n"
+      + "Cz2qViG3bZ7UAbSQowGwd8adMnt/D3GrKgt1FR+mZZHNCui0DEl4wsqX8CuYwy5m\n"
+      + "NmI+ui5Sq/VDeNhiMAvew+tmAcz4rGJWbsmF8NEufSS52iGBOLOfgcKDq4yZ/CTL\n"
+      + "XLhs9LIOOMejZcESMM69S3XE7mgkRumPJPshAgMBAAGjUzBRMB0GA1UdDgQWBBRt\n"
+      + "QxlUV3gH182o6L6ooYCsA4mOeDAfBgNVHSMEGDAWgBRtQxlUV3gH182o6L6ooYCs\n"
+      + "A4mOeDAPBgNVHRMBAf8EBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQATJv9Rbz7L\n"
+      + "E5iRxLaCttlVWDoPDKaKKLS2/W+knhnlO9dp6guNMg6ZdNj6JgYNktkqFRqFDwR+\n"
+      + "upWtBmY/IJboNavfbGiGOsg45H9scf9Iq0uH8zPCJKxJAfbQyqQ1zwTDWMeXkmEy\n"
+      + "mublHy0rlIrFRbjHpi05e6q2u2Efe2HEW0IVnmdXK44AJUu9umf2Zhy+iY/HBwm3\n"
+      + "jVmp1nU/Blm4ojXDR/oa0o+JRGuXFvJyKmODQQQoul5wME/p5TKw5JSX68t870o+\n"
+      + "8gG8sjBqWagKI9VsO5yBlEKGHjIlSmNjNEh5GfHwehMMAb0kO467MxuBP0iuNIhQ\n"
+      + "Bb+3Jm9l+t+3\n"
+      + "-----END CERTIFICATE-----";
 
-  private static final String TEST_KEY_PEM =
-      "-----BEGIN PRIVATE KEY-----\n" // gitleaks:allow
-          + "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCvyidJ8iaM93ym\n"
-          + "iTqH/GOZWzU7/bR6bYMK/czHiHZNLgmUARgE1rXWxsrQ5vrnVgHKkg49+AX84L+B\n"
-          + "Ajh/e4djzOquRWn/arw8oo7TWmmqFG1emkLD5qNqENdIRp22QdAd61FaXEOfhiKs\n"
-          + "24LqzKNl6W1s3JXEB0hVjw8Bc5IkjDBz8zgNCz2qViG3bZ7UAbSQowGwd8adMnt/\n"
-          + "D3GrKgt1FR+mZZHNCui0DEl4wsqX8CuYwy5mNmI+ui5Sq/VDeNhiMAvew+tmAcz4\n"
-          + "rGJWbsmF8NEufSS52iGBOLOfgcKDq4yZ/CTLXLhs9LIOOMejZcESMM69S3XE7mgk\n"
-          + "RumPJPshAgMBAAECggEAHOQZjvKoWVS3fNti7kSRzfuKhUCOL+zds6nAxS5tWwPs\n"
-          + "+REw9GKhYhor7Sd5Jxogkz1r781alpQNhVNrG7CesM1V6Kxp3luZclG5LY1Xfmd7\n"
-          + "DihGPtIiG2WHLE7GztSEMtag+TvetVIjUsuE+bgGRtKVdGXQi3Yy8jTf33oyE6Vc\n"
-          + "ZP7r6gaMfkF18NvDVrGaDNDzBg56V8jm5A1eR1dGyI1CF1JffmxM+MG+AxPm3nFd\n"
-          + "vMHhuJUf2p9iJ9OJV2DiH0aa7WHZs6W6LnVJLO9MNyMS53mzIvB6Ir0rY71hYM/o\n"
-          + "+NjsZjcY4nAWxPJrr01enw7jhFmEl8IokwjTC9J8DQKBgQDxZpLm4D2X6ZQBlrrV\n"
-          + "ScXHQZSYt9mNhxA0F8gXEg7EU84l8BcwA/UxiqIbQ5ZgQJz5cqqaqxHzLmZn7s2t\n"
-          + "rFgXlmzASvbQISIzOCP9lBgN7L67pMga4RLEp5YmXj5GcbVD6sZNAMUWsvD7n7Nj\n"
-          + "8GZTjQturKMpmn6TOYvxLYD17wKBgQC6a8X5Ee9ce3ohBfeKwlOtROhWmUZMJEsk\n"
-          + "m96+wuaC0Hd4PJTnk+XixhjagLVvumf8+llbISf9FKvKl4vTfO5IYsnHaAzrmnM5\n"
-          + "iWege09oqcdLsOjEDNa9DlIAJji9KjK0wthDqzssd/0r8ACDk/rXrfFVoB4LD1kH\n"
-          + "3rHLR2+v7wKBgQDexgpLhiVRS/2Kec9xheM/PwYi6YebVElfyZKdt8xo4e4clLTR\n"
-          + "WtWsIoP3JShhKz+x9F/83GJ0SR24TGkDHUkue2NhEc0zxWAKZITSPxjzwMdD443H\n"
-          + "QG5RjVbDkbVmzQi65K6b4VoA2KILlBW45OGJuWATgr4QZsLomvLKX3PjgQKBgCPD\n"
-          + "m1Z+T1qBwdtmBqiTQXRjXOOIQklHSTc7xA762zhLgDAMB1EqOZc5A/mfaa+Gpn6N\n"
-          + "FOl7we02swKJ8mRfemNeuHFOD/R/TA3Ew6GxJnng4IAv72f1QplAjxDCRG5eEx5E\n"
-          + "pzLMPez5FBQC1kZn9F6+WydfMYRTrHPrEFb8XHv3AoGBAN0QPbZzyCifKnBbVYGb\n"
-          + "DSH06tS+TDB8J6DTHFYXlrkkvhgDocNZ2dZkf02dtx9H4N6COMnoSum1J9k1a0is\n"
-          + "xecqZea5/fIwgvnGKEbnhqTul0yUcfBr5bWHb+RO9yy7jUcKVYDrCQ85hGqLZmxJ\n"
-          + "Xex4+CqzwGQzzbM1muriTGFY\n"
-          + "-----END PRIVATE KEY-----";
+  private static final String TEST_KEY_PEM = "-----BEGIN PRIVATE KEY-----\n" // gitleaks:allow
+      + "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCvyidJ8iaM93ym\n"
+      + "iTqH/GOZWzU7/bR6bYMK/czHiHZNLgmUARgE1rXWxsrQ5vrnVgHKkg49+AX84L+B\n"
+      + "Ajh/e4djzOquRWn/arw8oo7TWmmqFG1emkLD5qNqENdIRp22QdAd61FaXEOfhiKs\n"
+      + "24LqzKNl6W1s3JXEB0hVjw8Bc5IkjDBz8zgNCz2qViG3bZ7UAbSQowGwd8adMnt/\n"
+      + "D3GrKgt1FR+mZZHNCui0DEl4wsqX8CuYwy5mNmI+ui5Sq/VDeNhiMAvew+tmAcz4\n"
+      + "rGJWbsmF8NEufSS52iGBOLOfgcKDq4yZ/CTLXLhs9LIOOMejZcESMM69S3XE7mgk\n"
+      + "RumPJPshAgMBAAECggEAHOQZjvKoWVS3fNti7kSRzfuKhUCOL+zds6nAxS5tWwPs\n"
+      + "+REw9GKhYhor7Sd5Jxogkz1r781alpQNhVNrG7CesM1V6Kxp3luZclG5LY1Xfmd7\n"
+      + "DihGPtIiG2WHLE7GztSEMtag+TvetVIjUsuE+bgGRtKVdGXQi3Yy8jTf33oyE6Vc\n"
+      + "ZP7r6gaMfkF18NvDVrGaDNDzBg56V8jm5A1eR1dGyI1CF1JffmxM+MG+AxPm3nFd\n"
+      + "vMHhuJUf2p9iJ9OJV2DiH0aa7WHZs6W6LnVJLO9MNyMS53mzIvB6Ir0rY71hYM/o\n"
+      + "+NjsZjcY4nAWxPJrr01enw7jhFmEl8IokwjTC9J8DQKBgQDxZpLm4D2X6ZQBlrrV\n"
+      + "ScXHQZSYt9mNhxA0F8gXEg7EU84l8BcwA/UxiqIbQ5ZgQJz5cqqaqxHzLmZn7s2t\n"
+      + "rFgXlmzASvbQISIzOCP9lBgN7L67pMga4RLEp5YmXj5GcbVD6sZNAMUWsvD7n7Nj\n"
+      + "8GZTjQturKMpmn6TOYvxLYD17wKBgQC6a8X5Ee9ce3ohBfeKwlOtROhWmUZMJEsk\n"
+      + "m96+wuaC0Hd4PJTnk+XixhjagLVvumf8+llbISf9FKvKl4vTfO5IYsnHaAzrmnM5\n"
+      + "iWege09oqcdLsOjEDNa9DlIAJji9KjK0wthDqzssd/0r8ACDk/rXrfFVoB4LD1kH\n"
+      + "3rHLR2+v7wKBgQDexgpLhiVRS/2Kec9xheM/PwYi6YebVElfyZKdt8xo4e4clLTR\n"
+      + "WtWsIoP3JShhKz+x9F/83GJ0SR24TGkDHUkue2NhEc0zxWAKZITSPxjzwMdD443H\n"
+      + "QG5RjVbDkbVmzQi65K6b4VoA2KILlBW45OGJuWATgr4QZsLomvLKX3PjgQKBgCPD\n"
+      + "m1Z+T1qBwdtmBqiTQXRjXOOIQklHSTc7xA762zhLgDAMB1EqOZc5A/mfaa+Gpn6N\n"
+      + "FOl7we02swKJ8mRfemNeuHFOD/R/TA3Ew6GxJnng4IAv72f1QplAjxDCRG5eEx5E\n"
+      + "pzLMPez5FBQC1kZn9F6+WydfMYRTrHPrEFb8XHv3AoGBAN0QPbZzyCifKnBbVYGb\n"
+      + "DSH06tS+TDB8J6DTHFYXlrkkvhgDocNZ2dZkf02dtx9H4N6COMnoSum1J9k1a0is\n"
+      + "xecqZea5/fIwgvnGKEbnhqTul0yUcfBr5bWHb+RO9yy7jUcKVYDrCQ85hGqLZmxJ\n"
+      + "Xex4+CqzwGQzzbM1muriTGFY\n"
+      + "-----END PRIVATE KEY-----";
   // endregion
 
   private void mockSsmWithValidCredentials() {
